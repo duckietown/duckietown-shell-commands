1817180bdb90eb51379d1dc1799e1ee5d93d5ebf
Andrea Censi <andrea@nutonomy.com>
2018-09-02 23:42:48 +0800

    reorg

--- init_sd_card/__init__.py
+++ init_sd_card/__init__.py
@@ -2,17 +2,17 @@
 #
 # Maintainer: Andrea F. Daniele
 
+print('one')
 # import current command
-try: from .command import *
-except ImportError: pass
+from .command import *
 
 # import subcommands
-from os.path import dirname, basename, isfile, isdir
-import glob
+#from os.path import dirname, basename, isfile, isdir
+#import glob
 
-modules = glob.glob(dirname(__file__)+"/*")
+#modules = glob.glob(dirname(__file__)+"/*")
 
 # load submodules
-for mod in [m for m in modules if isdir(m)]:
-    try: exec( 'import %s' % basename(mod) )
-    except ImportError: pass
+#for mod in [m for m in modules if isdir(m)]:
+#    try: exec( 'import %s' % basename(mod) )
+#    except ImportError: pass
--- init_sd_card/command.py
+++ init_sd_card/command.py
@@ -2,17 +2,21 @@ from __future__ import print_function
 from dt_shell import DTCommandAbs
 from os.path import join, realpath, dirname
 import subprocess
-
+import os
 
 class DTCommand(DTCommandAbs):
 
     @staticmethod
     def command(shell, args):
-        script_file = join(dirname(realpath(__file__)), 'scripts', 'init_sd_card.sh')
+        script_file = join(dirname(realpath(__file__)), '..', 'init_sd_card.scripts', 'init_sd_card.sh')
+        if not os.path.exists(script_file):
+            msg = 'Could not find script %s' % script_file
+            raise Exception(msg)
         script_cmd = '/bin/bash %s' % script_file
         process = subprocess.Popen(script_cmd, shell=True, stdout=subprocess.PIPE)
         process.wait()
         if process.returncode == 0:
             print( 'Done!' )
         else:
-            print( 'An error occurred while initializing the SD card, please check and try again.' )
+            msg = ( 'An error occurred while initializing the SD card, please check and try again.' )
+            raise Exception(msg)
