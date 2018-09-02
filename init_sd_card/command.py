3c3bd3c75a1557d717dcbab185ff2ed74b417a93
Andrea Censi <acensi@ethz.ch>
2018-09-02 19:16:21 +0200

    fix

--- init_sd_card/command.py
+++ init_sd_card/command.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
-from dt_shell import DTCommandAbs
-from os.path import join, realpath, dirname
-import subprocess
+
 import os
+import subprocess
 import sys
+from os.path import join, realpath, dirname
+
+from dt_shell import DTCommandAbs
+
+
 class DTCommand(DTCommandAbs):
 
     @staticmethod
@@ -13,10 +17,10 @@ class DTCommand(DTCommandAbs):
             msg = 'Could not find script %s' % script_file
             raise Exception(msg)
         script_cmd = '/bin/bash %s' % script_file
-        subprocess.call(script_cmd, shell=True, stdin=sys.stdin, stderr=sys.stderr, stdout=sys.stdout)
-        #process.communicate()
-        if process.returncode == 0:
-            print( 'Done!' )
+        ret = subprocess.call(script_cmd, shell=True, stdin=sys.stdin, stderr=sys.stderr, stdout=sys.stdout)
+        # process.communicate()
+        if ret == 0:
+            print('Done!')
         else:
-            msg = ( 'An error occurred while initializing the SD card, please check and try again (%s).'% process.returncode )
+            msg = ('An error occurred while initializing the SD card, please check and try again (%s).' % ret)
             raise Exception(msg)
