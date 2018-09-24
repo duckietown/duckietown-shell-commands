6f0ee1321712075be3393b96fbd990ab95854b01
breandan <bre@ndan.co>
2018-09-24 12:53:42 -0400

    pass DUCKIEBOT_NAME into container

--- start_gui_tools/start_gui_tools.sh
+++ start_gui_tools/start_gui_tools.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-MDNS_ALIAS="$1"
-DUCKIEBOT_IP="$2"
+DUCKIEBOT_NAME="$1"
+MDNS_ALIAS="$2"
+DUCKIEBOT_IP="$3"
 
 platform='unknown'
 unamestr=$(uname)
@@ -15,9 +16,9 @@ docker pull duckietown/rpi-gui-tools
 
 if [[ $platform == 'linux' ]]; then
   xhost +
-  docker run -it --net host --privileged --env ROS_MASTER=$1 --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" duckietown/rpi-gui-tools /bin/bash -c "echo $DUCKIEBOT_IP $MDNS_ALIAS | sudo tee -a /etc/hosts && bash"
+  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" duckietown/rpi-gui-tools /bin/bash -c "echo $DUCKIEBOT_IP $MDNS_ALIAS | sudo tee -a /etc/hosts && bash"
 elif [[ $platform == 'macos' ]]; then
   IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
   xhost +$IP
-  docker run -it --net host --privileged --env ROS_MASTER=$1 --env="QT_X11_NO_MITSHM=1" -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix duckietown/rpi-gui-tools  /bin/bash -c "echo $DUCKIEBOT_IP $MDNS_ALIAS | sudo tee -a /etc/hosts && bash"
+  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env="QT_X11_NO_MITSHM=1" -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix duckietown/rpi-gui-tools  /bin/bash -c "echo $DUCKIEBOT_IP $MDNS_ALIAS | sudo tee -a /etc/hosts && bash"
 fi
