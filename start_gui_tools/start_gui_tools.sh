62d72668a71fc28d3b17ceda357efbc87367b788
Andrea Censi <AndreaCensi@users.noreply.github.com>
2018-10-01 19:35:46 +0200

    Update start_gui_tools.sh

--- start_gui_tools/start_gui_tools.sh
+++ start_gui_tools/start_gui_tools.sh
@@ -15,9 +15,9 @@ docker pull duckietown/rpi-gui-tools
 
 if [[ $platform == 'linux' ]]; then
   xhost +
-  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" duckietown/rpi-gui-tools
+  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" duckietown/rpi-gui-tools:master18
 elif [[ $platform == 'macos' ]]; then
   IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
   xhost +$IP
-  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env="QT_X11_NO_MITSHM=1" -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix duckietown/rpi-gui-tools
+  docker run -it --net host --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env="QT_X11_NO_MITSHM=1" -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix duckietown/rpi-gui-tools:master18
 fi
