8acc14cdf89c15987db2494989f514ccaf5ea3cc
Andrea Censi <acensi@ethz.ch>
2018-10-07 16:08:26 +0200

    build

--- challenges/define/command.py
+++ challenges/define/command.py
@@ -1,9 +1,12 @@
 import argparse
+import datetime
 import os
 
 import yaml
 from dt_shell import DTCommandAbs
+from dt_shell.env_checks import get_dockerhub_username
 from dt_shell.remote import dtserver_challenge_define
+from duckietown_data_server import dslogger
 
 
 class DTCommand(DTCommandAbs):
@@ -21,8 +24,12 @@ class DTCommand(DTCommandAbs):
         parser = argparse.ArgumentParser()
         parser.add_argument('--config', required=True,
                             help="YAML configuration file")
-        parser.add_argument('--make-sha', dest='make_sha', default=False, action='store_true')
+
+        parser.add_argument('--build', default=None, help="try `evaluator:.`")
+
         parsed = parser.parse_args(args)
+
+        no_cache = False
 
         fn = os.path.join(parsed.config)
         if not os.path.exists(fn):
@@ -35,11 +42,53 @@ class DTCommand(DTCommandAbs):
 
         challenge = ChallengeDescription.from_yaml(data)
 
-        if parsed.make_sha:
-            for step in challenge.steps.values():
-                step.update_container()
+        if parsed.build:
+            import docker
+            client = docker.from_env()
+            builds = parsed.build.split(',')
+            for build in builds:
+                service_to_build, dirname = build.split(':')
+                dslogger.info('building for service %s in dir %s' % (service_to_build, dirname))
+
+                image, tag, repo_only, tag_only = build_image(client, dirname, challenge.name, no_cache)
+                dslogger.info('Pushing %s' % tag)
+                for line in client.images.push(repo_only, tag_only, stream=True):
+                    print(line)
+
+                nchanged = 0
+                for step in challenge.steps.values():
+                    services = step.evaluation_parameters.services
+                    for service_name, service in services.items():
+                        if service_name == service_to_build:
+                            dslogger.info('Using %s = %s' % (service_name, tag))
+                            service.image = tag
+                            nchanged += 1
+                if nchanged == 0:
+                    msg = 'Could not find service %s' % service_to_build
+                    raise Exception(msg)
 
         data2 = yaml.dump(challenge.as_dict())
 
         challenge_id = dtserver_challenge_define(token, data2)
         print('created challenge %s' % challenge_id)
+
+def build_image(client, path, challenge_name,  no_cache=False):
+
+    d = datetime.datetime.now()
+    username = get_dockerhub_username()
+    tag_only = tag_from_date(d)
+    repo_only = '%s/%s-evaluator' % (username, challenge_name)
+    tag = '%s:%s' % (repo_only, tag_only)
+    image = client.images.build(path=path, nocache=no_cache, tag=tag)
+    return image, tag, repo_only, tag_only
+
+
+def tag_from_date(d):
+    # YYYY-MM-DDTHH:MM:SS[.mmmmmm][+HH:MM].
+    s = d.isoformat()
+
+    s = s.replace(':', '_')
+    s = s.replace('T', '_')
+    s = s.replace('-', '_')
+    s = s[:s.index('.')]
+    return s
