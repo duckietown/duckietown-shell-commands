69542b8ab127f08ffdd9a58efe340500b815130b
Andrea Censi <acensi@ethz.ch>
2018-10-17 13:19:02 +0200

    Bump version: 1.0.4 → 1.0.5

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-duckietown_shell_commands_version = '1.0.4'
+duckietown_shell_commands_version = '1.0.5'
 import sys
 
 sys.stderr.write('Duckietown Shell commands version %s\n' % duckietown_shell_commands_version)
