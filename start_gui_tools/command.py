5867a5df9918dcf767f1b7f645f215f6bef7a2cc
breandan <bre@ndan.co>
2018-10-17 02:25:46 -0400

    migrate start_gui_tools to docker-py and argparse

--- start_gui_tools/command.py
+++ start_gui_tools/command.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 
+import argparse
 import os
+import platform
 import subprocess
 import sys
-from os.path import join, realpath, dirname
+from os.path import join, realpath, dirname, expanduser
+from subprocess import call, check_output
 
 from dt_shell import DTCommandAbs, dtslogger
 from utils.networking import get_duckiebot_ip
@@ -13,15 +16,17 @@ class DTCommand(DTCommandAbs):
 
     @staticmethod
     def command(shell, args):
-        script_file = join(dirname(realpath(__file__)), 'start_gui_tools.sh')
+        prog = 'dts keyboard_control DUCKIEBOT_NAME'
+        usage = """
+Keyboard control: 
 
-        if len(args) < 1:
-            raise Exception('No Duckiebot name received, aborting!')
+    %(prog)s
+"""
 
-        duckiebot_ip = get_duckiebot_ip(duckiebot_name=args[0])
-
-        script_cmd = '/bin/bash %s %s %s' % (script_file, args[0], duckiebot_ip)
-        print('Running %s' % script_cmd)
+        parser = argparse.ArgumentParser(prog=prog, usage=usage)
+        parser.add_argument('hostname', default=None, help='Name of the Duckiebot to calibrate')
+        parser.add_argument('--debug', dest='debug', action='store_true', default=False)
+        parsed_args = parser.parse_args(args)
 
         env = {}
         env.update(os.environ)
@@ -31,10 +36,37 @@ class DTCommand(DTCommandAbs):
             dtslogger.info(msg)
             env.pop(V)
 
-        ret = subprocess.call(script_cmd, shell=True, stdin=sys.stdin, stderr=sys.stderr, stdout=sys.stdout, env=env)
-        # process.communicate()
-        if ret == 0:
-            print('Done!')
-        else:
-            msg = ('An error occurred while starting the GUI tools container, please check and try again (%s).' % ret)
-            raise Exception(msg)
+        shell.start_gui_tools(parsed_args.hostname)
+
+    def start_gui_tools(self, duckiebot_name):
+        duckiebot_ip = get_duckiebot_ip(duckiebot_name)
+        import docker
+        local_client = docker.from_env()
+        operating_system = platform.system()
+
+        IMAGE_RPI_GUI_TOOLS = 'duckietown/rpi-gui-tools:master18'
+
+        local_client.images.pull(IMAGE_RPI_GUI_TOOLS)
+
+        env_vars = {
+            'ROS_MASTER': duckiebot_name,
+            'DUCKIEBOT_NAME': duckiebot_name,
+            'DUCKIEBOT_IP': duckiebot_ip,
+            'QT_X11_NO_MITSHM': True,
+            'DISPLAY': True
+        }
+
+        if operating_system == 'Linux':
+            call(["xhost", "+"])
+            local_client.containers.run(image=IMAGE_RPI_GUI_TOOLS,
+                                        network_mode='host',
+                                        privileged=True,
+                                        env_vars=env_vars)
+        if operating_system == 'Darwin':
+            IP = check_output(['/bin/sh', '-c', 'ifconfig en0 | grep inet | awk \'$1=="inet" {print $2}\''])
+            env_vars['IP'] = IP
+            call(["xhost", "+IP"])
+            local_client.containers.run(image=IMAGE_RPI_GUI_TOOLS,
+                                        network_mode='host',
+                                        privileged=True,
+                                        env_vars=env_vars)
