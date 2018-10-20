26fdd1da2082b58be886e7d9d3ea45a686b01839
aroumie1997 <43572412+aroumie1997@users.noreply.github.com>
2018-10-21 01:55:55 +0200

    Ros-picam check

--- calibrate_intrinsics/calibrate_duckiebot.sh
+++ calibrate_intrinsics/calibrate_duckiebot.sh
@@ -17,6 +17,13 @@ IMAGE_BASE=duckietown/rpi-duckiebot-base:master18
 docker pull $IMAGE_CALIBRATION
 docker -H "$DUCKIEBOT_NAME.local" pull $IMAGE_BASE
 
+if [ $(docker -H "$DUCKIEBOT_NAME.local" inspect -f '{{.State.Running}}' $ros-picam) = "true" ]; then 
+   echo "ros-picam container is running, and will now be stopped"
+#   docker -H "$DUCKIEBOT_NAME.local" stop ros-picam
+else 
+   echo "ros-picam container was not already running"
+fi
+
 if [[ $platform == 'linux' ]]; then
   xhost +
   docker run -it --net host -v /data:/data --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env="DISPLAY" --env QT_X11_NO_MITSHM=1 $IMAGE_CALIBRATION
@@ -24,11 +31,4 @@ elif [[ $platform == 'macos' ]]; then
   IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
   xhost +$IP
   docker run -it --net host -v $HOME/data:/data --privileged --env ROS_MASTER=$DUCKIEBOT_NAME --env DUCKIEBOT_NAME=$DUCKIEBOT_NAME --env DUCKIEBOT_IP=$DUCKIEBOT_IP --env QT_X11_NO_MITSHM=1 -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix $IMAGE_CALIBRATION
-fi
-
-if [ $(docker -H "$DUCKIEBOT_NAME.local" inspect -f '{{.State.Running}}' $ros-picam) = "true" ]; then 
-   echo "ros-picam container is running, and will now be stopped"
-   docker -H "$DUCKIEBOT_NAME.local" stop ros-picam
-else 
-   echo "ros-picam container was not already running"
 fi
