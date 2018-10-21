535b0712697aaa94878b09ac6d3a4b944d52d187
aroumie1997 <43572412+aroumie1997@users.noreply.github.com>
2018-10-21 08:51:30 +0200

    Split calibration (#57)
    * Separate Folders Added for Each Calibration Command
    
    * Removed parts relating to extrinsics and wheels
    
    * Removed parts related to extrinsics and wheels
    
    * Removed parts related to intrinsics and wheels
    
    * Removed parts related to intrinsics and wheels
    
    * Removed parts related to intrinsics and extrinsics
    
    * Removed parts related to intrinsics and extrinsics
    
    * Added rod-picam container check
    
    * Added ros-picam container check
    
    * Ros-picam check
    
    * ros-picam check
    
    * ros-picam check
    
    * ros-picam check
    
    * ros-picam check
    
    * Added ros-picam check
    
    * Added ros-picam check
    
    * added ros-picam check
    
    * added ros-picam check
    
    * added ros-picam check
    
    * minor fix and ros-picam check

---
+++ calibrate_extrinsics/calibrate_duckiebot.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+DUCKIEBOT_NAME="$1"
+DUCKIEBOT_IP="$2"
+
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='macos'
+fi
+ 
+IMAGE_CALIBRATION=duckietown/rpi-duckiebot-calibration:master18
+IMAGE_BASE=duckietown/rpi-duckiebot-base:master18
+
+docker pull $IMAGE_CALIBRATION
+docker -H "$DUCKIEBOT_NAME.local" pull $IMAGE_BASE
+
+if [ $(docker -H "$DUCKIEBOT_NAME.local" inspect -f '{{.State.Running}}' 'ros-picam') == "true" ]; then 
+   echo "********************"
+   echo "ros-picam container is running, and will now be stopped"
+   docker -H "$DUCKIEBOT_NAME.local" stop ros-picam
+else 
+   echo "********************"
+   echo "ros-picam container was not already running"
+fi
+echo "********************"
+
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+NAME="out-calibrate-extrinsics-$DUCKIEBOT_NAME-$TIMESTAMP"
+#SNAME="out-simulation-$DUCKIEBOT_NAME-$TIMESTAMP"
+VNAME="out-pipeline-$DUCKIEBOT_NAME-$TIMESTAMP"
+
+echo "********************"
+echo "Place the Duckiebot on the calibration patterns and press ENTER."
+read
+ 
+docker -H "$DUCKIEBOT_NAME.local" run -it  --privileged -v /data:/data --net host $IMAGE_BASE /bin/bash -c "source /home/software/docker/env.sh && rosrun complete_image_pipeline calibrate_extrinsics -o /data/$NAME > /data/$NAME.log"
+ 
+
+
+#echo "Running Simulated Verification"
+
+
+#docker -H "$DUCKIEBOT_NAME.local" run -it  --privileged -v /data:/data --net host duckietown/rpi-duckiebot-base /bin/bash -c "source /home/software/docker/env.sh && rosrun complete_image_pipeline validate_calibration -o /data/$SNAME > /data/$SNAME.log"
+
+
+echo "********************"
+echo "Place the Duckiebot in a lane and press ENTER."
+read
+
+docker -H "$DUCKIEBOT_NAME.local" run -it  --privileged -v /data:/data --net host $IMAGE_BASE /bin/bash -c "source /home/software/docker/env.sh && rosrun complete_image_pipeline single_image_pipeline -o /data/$VNAME > /data/$VNAME.log"
+
---
+++ calibrate_wheels/command.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+
+import argparse
+import datetime
+import os
+import platform
+import subprocess
+import sys
+from os.path import join, realpath, dirname, expanduser
+from subprocess import call
+
+from dt_shell import DTCommandAbs, dtslogger
+from utils.networking import get_duckiebot_ip
+
+
+class DTCommand(DTCommandAbs):
+
+    @staticmethod
+    def command(shell, args):
+        script_file = join(dirname(realpath(__file__)), 'calibrate_duckiebot.sh')
+
+        prog = 'dts calibrate_wheels DUCKIEBOT_NAME'
+        usage = """
+Calibrate: 
+
+    %(prog)s
+"""
+
+        parser = argparse.ArgumentParser(prog=prog, usage=usage)
+        parser.add_argument('hostname', default=None, help='Name of the Duckiebot to calibrate')
+        parsed_args = parser.parse_args(args)
+
+        duckiebot_ip = get_duckiebot_ip(parsed_args.hostname)
+        # shell.calibrate(duckiebot_name=args[0], duckiebot_ip=duckiebot_ip)
+        script_cmd = '/bin/bash %s %s %s' % (script_file, parsed_args.hostname, duckiebot_ip)
+
+        env = {}
+        env.update(os.environ)
+        V = 'DOCKER_HOST'
+        if V in env:
+            msg = 'I will ignore %s because the calibrate command knows what it\'s doing.' % V
+            dtslogger.info(msg)
+            env.pop(V)
+
+        ret = call(script_cmd, shell=True, stdin=sys.stdin, stderr=sys.stderr, stdout=sys.stdout, env=env)
+
+        if ret == 0:
+            print('Done!')
+        else:
+            msg = ('An error occurred while running the calibration procedure, please check and try again (%s).' % ret)
+            raise Exception(msg)
+
+    def calibrate(self, duckiebot_name, duckiebot_ip):
+        import docker
+        local_client = docker.from_env()
+        duckiebot_client = docker.DockerClient('tcp://' + duckiebot_ip + ':2375')
+        operating_system = platform.system()
+
+        IMAGE_CALIBRATION = 'duckietown/rpi-duckiebot-calibration:master18'
+        IMAGE_BASE = 'duckietown/rpi-duckiebot-base:master18'
+
+        duckiebot_client.images.pull(IMAGE_BASE)
+        local_client.images.pull(IMAGE_CALIBRATION)
+        user_home = expanduser("~")
+        datavol = {'%s/data' % user_home: {'bind': '/data'}}
+
+        env_vars = {
+            'ROS_MASTER': duckiebot_name,
+            'DUCKIEBOT_NAME': duckiebot_name,
+            'DUCKIEBOT_IP': duckiebot_ip,
+            'QT_X11_NO_MITSHM': True
+        }
+
+        print("********************")
+        print("To perform the wheel calibration, follow the steps described in the Duckiebook.")
+        print("http://docs.duckietown.org/DT18/opmanual_duckiebot/out/wheel_calibration.html")
+        raw_input("You will now be given a container running on the Duckiebot for wheel calibration.")
+
+
+        duckiebot_client.containers.run(image=IMAGE_CALIBRATION,
+                                        privileged=True,
+                                        network_mode='host',
+                                        datavol={'/data': {'bind': '/data'}},
+                                        command='/bin/bash')
