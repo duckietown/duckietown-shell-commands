2b3c5016888a301749f29a221bf5cbc74f656ead
breandan <bre@ndan.co>
2018-12-10 12:48:34 -0500

    downloading logs would be nice...

--- logs/download/command.py
+++ logs/download/command.py
@@ -1,8 +1,25 @@
+import argparse
+
 from dt_shell import DTCommandAbs
+
+usage = """
+
+    ## Basic usage
+
+        Downloads the logs from a host.
+
+            $ dts logs download HOSTNAME
+
+    """
 
 
 class DTCommand(DTCommandAbs):
-
     @staticmethod
     def command(shell, args):
-        pass
+        prog = 'dts duckiebot evaluate'
+        parser = argparse.ArgumentParser(prog=prog, usage=usage)
+
+        parser.add_argument('hostname', default=None,
+                            help="Name of the host where logs should be fetched")
+
+        parsed = parser.parse_args(args)
