adbf0eb2c2e876e788c85ecbbc6257161e483198
Andrea Censi <acensi@ethz.ch>
2019-04-15 12:38:47 +0200

    Bump version: 4.0.10 → 4.0.11

--- update/__init__.py
+++ update/__init__.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 6):
 
 
 min_duckietown_shell = '4.0.14'
-duckietown_shell_commands_version = '4.0.10'
+duckietown_shell_commands_version = '4.0.11'
 
 
 def parse_version(x):
