79af390a8de175bd67bcf8f0b096074184d4baea
Liam Paull <paulll@iro.umontreal.ca>
2019-05-01 22:45:21 -0400

    addig mem limits

--- duckiebot/demo/command.py
+++ duckiebot/demo/command.py
@@ -1,10 +1,11 @@
 import argparse
 import docker
+import threading
 
 from dt_shell import DTCommandAbs, dtslogger
 from dt_shell.env_checks import check_docker_environment
 
-from utils.docker_utils import default_env, bind_duckiebot_data_dir, remove_if_running
+from utils.docker_utils import default_env, bind_duckiebot_data_dir, remove_if_running, continuously_monitor
 from utils.networking_utils import get_duckiebot_ip
 
 usage = """
@@ -26,8 +27,6 @@ class DTCommand(DTCommandAbs):
         prog = 'dts duckiebot demo'
         parser = argparse.ArgumentParser(prog=prog, usage=usage)
 
-        group = parser.add_argument_group('Basic')
-
         parser.add_argument('--demo_name', dest="demo_name", default=None,
                             help="Name of the demo to run")
 
@@ -37,7 +36,7 @@ class DTCommand(DTCommandAbs):
         parser.add_argument('--package_name', dest="package_name", default="duckietown",
                             help="You can specify the package that you want to use to look for launch files")
 
-        group.add_argument('--image', dest="image_to_run",
+        parser.add_argument('--image', dest="image_to_run",
                            default="duckietown/rpi-duckiebot-base:master19",
                            help="Docker image to use, you probably don't need to change ",)
 
@@ -70,11 +69,17 @@ class DTCommand(DTCommandAbs):
         cmd = 'roslaunch %s %s.launch veh:=%s' % (package_name, demo_name, duckiebot_name)
         dtslogger.info("Running command %s" % cmd)
 
-        duckiebot_client.containers.run(image=image_base,
+        demo_container = duckiebot_client.containers.run(image=image_base,
                                         command= cmd,
                                         network_mode='host',
                                         volumes=bind_duckiebot_data_dir(),
                                         privileged=True,
                                         name=container_name,
+                                        mem_limit='800m',
+                                        memswap_limit='2800m',
+                                        tty=True,
+                                        detach=True,
                                         environment=env_vars)
 
+        monitor_thread = threading.Thread(target=continuously_monitor,args=(duckiebot_client, demo_container))
+        monitor_thread.start()
