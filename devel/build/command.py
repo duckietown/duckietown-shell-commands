441960f59252c39f97a00c63d095352b131486b9
Andrea F. Daniele <afdaniele@ttic.edu>
2019-08-20 10:40:21 -0500

    added `build` commands

---
+++ devel/build/command.py
@@ -0,0 +1,130 @@
+import os
+import argparse
+import subprocess
+import io, sys
+import getpass
+from .image_analyzer import ImageAnalyzer
+from dt_shell import DTCommandAbs, dtslogger
+
+DEFAULT_ARCH='arm32v7'
+DEFAULT_MACHINE='unix:///var/run/docker.sock'
+
+
+class DTCommand(DTCommandAbs):
+
+    help = 'Builds the current project'
+
+
+    @staticmethod
+    def command(shell, args):
+        # configure arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-C', '--workdir', default=None,
+                            help="Directory containing the project to build")
+        parser.add_argument('-a', '--arch', default=DEFAULT_ARCH,
+                            help="Target architecture for the image to build")
+        parser.add_argument('-H', '--machine', default=DEFAULT_MACHINE,
+                            help="Docker socket or hostname where to build the image")
+        parser.add_argument('--pull', default=False, action='store_true',
+                            help="Whether to pull the latest base image used by the Dockerfile")
+        parser.add_argument('--no-cache', default=False, action='store_true',
+                            help="Whether to use the Docker cache")
+        parser.add_argument('-f', '--force', default=False, action='store_true',
+                            help="Whether to force the build when the git index is not clean")
+        parser.add_argument('--push', default=False, action='store_true',
+                            help="Whether to push the resulting image")
+        parsed, _ = parser.parse_known_args(args=args)
+        # ---
+        code_dir = parsed.workdir if parsed.workdir else os.getcwd()
+        dtslogger.info('Project workspace: {}'.format(code_dir))
+        # show info about project
+        shell.include.devel.info.command(shell, args)
+        # get info about current repo
+        repo_info = shell.include.devel.info.get_repo_info(code_dir)
+        repo = repo_info['REPOSITORY']
+        branch = repo_info['BRANCH']
+        nmodified = repo_info['INDEX_NUM_MODIFIED']
+        nadded = repo_info['INDEX_NUM_ADDED']
+        # check if the index is clean
+        if nmodified + nadded > 0:
+            dtslogger.warning('Your index is not clean (some files are not committed).')
+            dtslogger.warning('If you know what you are doing, use --force to force the execution of the command.')
+            if not parsed.force:
+                exit(1)
+            dtslogger.warning('Forced!')
+        # create defaults
+        default_tag = "duckietown/%s:%s" % (repo, branch)
+        tag = "duckietown/%s:%s-%s" % (repo, branch, parsed.arch)
+        # register bin_fmt in the target machine
+        _run_cmd([
+            'docker',
+                '-H=%s' % parsed.machine,
+                'run',
+                    '--rm',
+                    '--privileged',
+                    'multiarch/qemu-user-static:register',
+                    '--reset'
+        ])
+        # build
+        buildlog = _run_cmd([
+            'docker',
+                '-H=%s' % parsed.machine,
+                'build',
+                    '--pull=%d' % int(parsed.pull),
+                    '--no-cache=%d' % int(parsed.no_cache),
+                    '-t', tag,
+                    '--build-arg', 'ARCH={}'.format(parsed.arch),
+                    code_dir
+        ], True, True)
+        # get image history
+        historylog = _run_cmd([
+            'docker',
+                '-H=%s' % parsed.machine,
+                'history',
+                    '-H=false',
+                    '--format',
+                    '{{.ID}}:{{.Size}}',
+                    tag
+        ], True)
+        historylog = [l.split(':') for l in historylog if len(l.strip()) > 0]
+        # run docker image analysis
+        ImageAnalyzer.process(buildlog, historylog, codens=100)
+        # image tagging
+        if parsed.arch == DEFAULT_ARCH:
+            dtslogger.info("Tagging image {} as {}.".format(tag, default_tag))
+            _run_cmd([
+                'docker',
+                    '-H=%s' % parsed.machine,
+                    'tag',
+                        tag,
+                        default_tag
+            ])
+        # perform push (if needed)
+        if parsed.push:
+            shell.include.devel.push.command(shell, args)
+
+
+    @staticmethod
+    def complete(shell, word, line):
+        return []
+
+
+def _run_cmd(cmd, get_output=False, print_output=False):
+    dtslogger.debug('$ %s' % cmd)
+    if get_output:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        lines = []
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            line = line.rstrip()
+            if print_output:
+                print(line)
+            if line:
+                lines.append(line)
+        proc.wait()
+        if proc.returncode != 0:
+            msg = 'The command {} returned exit code {}'.format(cmd, proc.returncode)
+            dtslogger.error(msg)
+            raise RuntimeError(msg)
+        return lines
+    else:
+        subprocess.check_call(cmd)
---
+++ devel/build/image_analyzer.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import argparse
+import subprocess
+from termcolor import colored
+
+LAYER_SIZE_THR_YELLOW = 50 * 1024**2  # 50 MB
+LAYER_SIZE_THR_RED = 200 * 1024**2    # 200 MB
+
+
+class ImageAnalyzer(object):
+
+    @staticmethod
+    def about():
+        print()
+        print('='*30)
+        print(colored('Docker Build Analyzer', 'white', 'on_blue'))
+        print('Maintainer: Andrea F. Daniele (afdaniele@ttic.edu)')
+        print('='*30)
+        print()
+
+    @staticmethod
+    def sizeof_fmt(num, suffix='B'):
+        for unit in ['','K','M','G','T','P','E','Z']:
+            if abs(num) < 1024.0:
+                return "%3.2f %s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.2f%s%s" % (num, 'Yi', suffix)
+
+    @staticmethod
+    def process(buildlog, historylog, codens=0):
+        lines = buildlog
+        image_history = historylog
+        sizeof_fmt = ImageAnalyzer.sizeof_fmt
+
+        # return if the log is empty
+        if not lines:
+            raise ValueError('The build log is empty')
+
+        # return if the image history is empty
+        if not image_history:
+            raise ValueError('The image history is empty')
+
+        # define RegEx patterns
+        step_pattern = re.compile("Step ([0-9]+)/([0-9]+) : (.*)")
+        layer_pattern = re.compile(" ---> ([0-9a-z]{12})")
+        final_layer_pattern = re.compile("Successfully tagged (.*)")
+
+        # check if the build process succeded
+        if not final_layer_pattern.match(lines[-1]):
+            exit(codens+2)
+        image = final_layer_pattern.match(lines[-1]).group(1)
+
+        print()
+        ImageAnalyzer.about()
+
+        # find "Step XY/TOT" lines
+        steps_idx = [i for i in range(len(lines)) if step_pattern.match(lines[i])] + [len(lines)]
+
+        # create map {layerid: size_bytes}
+        layer_to_size_bytes = dict()
+        for l in image_history:
+            layerid, layersize = l
+            if layerid == 'missing':
+                continue
+            layer_to_size_bytes[layerid] = int(layersize)
+
+        # for each Step, find the layer ID
+        first_layer = None
+        last_layer = None
+        for i,j in zip(steps_idx, steps_idx[1:]):
+            indent_str = '|'
+            layerid_str = 'Layer ID:'
+            size_str = 'Size:'
+            cur_step_lines = lines[i:j]
+            open_layers = [layer_pattern.match(l) for l in cur_step_lines if layer_pattern.match(l)]
+            # get Step info
+            print('-' * 22)
+            stepline = lines[i]
+            stepno = step_pattern.match(stepline).group(1)
+            steptot = step_pattern.match(stepline).group(2)
+            stepcmd = re.sub(' +', ' ', step_pattern.match(stepline).group(3))
+            # get info about layer ID and size
+            layerid = None
+            layersize = 'ND'
+            bg_color = 'white'
+            fg_color = 'grey'
+            if len(open_layers) > 0:
+                layerid = open_layers[0].group(1)
+                if first_layer is None:
+                    first_layer = layerid
+                last_layer = layerid
+            # ---
+            if layerid in layer_to_size_bytes:
+                layersize = sizeof_fmt(layer_to_size_bytes[layerid])
+                fg_color = 'white'
+                bg_color = 'yellow' if layer_to_size_bytes[layerid] > LAYER_SIZE_THR_YELLOW else 'green'
+                bg_color = 'red' if layer_to_size_bytes[layerid] > LAYER_SIZE_THR_RED else bg_color
+                bg_color = 'blue' if stepcmd.startswith('FROM') else bg_color
+
+            indent_str = colored(indent_str, fg_color, 'on_'+bg_color)
+            size_str = colored(size_str, fg_color, 'on_'+bg_color)
+            layerid_str = colored(layerid_str, fg_color, 'on_'+bg_color)
+            # print info about the current layer
+            print(
+                '%s %s\n%sStep: %s/%s\n%sCommand: \n%s\t%s\n%s%s %s' % (
+                layerid_str, layerid,
+                indent_str, stepno, steptot,
+                indent_str, indent_str, stepcmd,
+                indent_str, size_str, layersize
+            ))
+            print()
+
+        # compute size of the base image
+        first_layer_idx = [i for i in range(len(image_history)) if image_history[i][0] == first_layer][0]
+        base_image_size = sum([int(l[1]) for l in image_history[first_layer_idx:]])
+
+        # compute size of the final image
+        final_image_size = sum([int(l[1]) for l in image_history])
+
+        # print info about the whole image
+        print()
+        print('Legend: %s %s\t%s %s\t%s < %s\t%s < %s\t%s > %s\t' % (
+            colored(' '*2, 'white', 'on_white'), 'EMPTY LAYER',
+            colored(' '*2, 'white', 'on_blue'), 'BASE SIZE',
+            colored(' '*2, 'white', 'on_green'), sizeof_fmt(LAYER_SIZE_THR_YELLOW),
+            colored(' '*2, 'white', 'on_yellow'), sizeof_fmt(LAYER_SIZE_THR_RED),
+            colored(' '*2, 'white', 'on_red'), sizeof_fmt(LAYER_SIZE_THR_RED)
+        ))
+        print()
+        print('=' * 22)
+        print('Final image name: %s' % image)
+        print('Final image size: %s' % sizeof_fmt(final_image_size))
+        print('Base image size: %s' % sizeof_fmt(base_image_size))
+        print('Your image added %s to the base image.' % sizeof_fmt(final_image_size-base_image_size))
+        print('=' * 22)
+        print()
+        print(colored('IMPORTANT', 'white', 'on_blue') + ': Always ask yourself, can I do better than that? ;)')
+        print()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--machine', default="", help="Hostname of the machine running the build")
+    parsed = parser.parse_args()
+
+    # put lines from the pipe in a list
+    lines = [l for l in sys.stdin if len(l.strip()) > 0]
+
+    # return if the log is empty
+    if not lines:
+        exit(1)
+
+    # handle remote build
+    docker_H = []
+    if len(parsed.machine) > 0:
+        docker_H = ['-H', parsed.machine]
+
+
+    # check if the build process succeded
+    if not final_layer_pattern.match(lines[-1]):
+        exit(2)
+
+    # get layers size from docker
+    image_history = subprocess.check_output(
+        ['docker'] + docker_H + ['history', '-H=false', '--format', '{{.ID}}:{{.Size}}', image],
+    ).decode('utf-8')
+    image_history = [l.split(':') for l in image_history.split('\n') if len(l.strip()) > 0]
+
+    # run image analysis
+    ImageAnalyzer.process(lines, image_history)
