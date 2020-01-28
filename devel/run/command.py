4d7be66db3a9c50fbfb026739a7e9e0949bb00aa
Andrea F. Daniele <afdaniele@ttic.edu>
2020-01-28 17:58:07 -0600

    added devel/run command

---
+++ devel/run/command.py
@@ -0,0 +1,241 @@
+import os
+import json
+import argparse
+import subprocess
+from dt_shell import DTCommandAbs, dtslogger
+
+
+DEFAULT_ARCH = 'arm32v7'
+DEFAULT_MACHINE = 'unix:///var/run/docker.sock'
+DOCKER_INFO = """
+Docker Endpoint:
+  Hostname: {Name}
+  Operating System: {OperatingSystem}
+  Kernel Version: {KernelVersion}
+  OSType: {OSType}
+  Architecture: {Architecture}
+  Total Memory: {MemTotal}
+  CPUs: {NCPU}
+"""
+ARCH_MAP = {
+    'arm32v7': ['arm', 'arm32v7', 'armv7l', 'armhf'],
+    'amd64': ['x64', 'x86_64', 'amd64', 'Intel 64'],
+    'arm64v8': ['arm64', 'arm64v8', 'armv8', 'aarch64']
+}
+CANONICAL_ARCH = {
+    'arm': 'arm32v7',
+    'arm32v7': 'arm32v7',
+    'armv7l': 'arm32v7',
+    'armhf': 'arm32v7',
+    'x64': 'amd64',
+    'x86_64': 'amd64',
+    'amd64': 'amd64',
+    'Intel 64': 'amd64',
+    'arm64': 'arm64v8',
+    'arm64v8': 'arm64v8',
+    'armv8': 'arm64v8',
+    'aarch64': 'arm64v8'
+}
+ARCH_COMPATIBILITY_MAP = {
+    'arm32v7': ['arm32v7'],
+    'arm64v8': ['arm32v7', 'arm64v8'],
+    'amd64': ['amd64']
+}
+DOCKER_LABEL_DOMAIN = "org.duckietown.label"
+
+
+class DTCommand(DTCommandAbs):
+
+    help = 'Runs the current project'
+
+
+    @staticmethod
+    def command(shell, args):
+        # configure arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-C', '--workdir', default=os.getcwd(),
+                            help="Directory containing the project to run")
+        parser.add_argument('-a', '--arch', default=DEFAULT_ARCH, choices=set(CANONICAL_ARCH.values()),
+                            help="Target architecture for the image to run")
+        parser.add_argument('-H', '--machine', default=DEFAULT_MACHINE,
+                            help="Docker socket or hostname where to run the image")
+        parser.add_argument('-n', '--name', default=None,
+                            help="Name of the container")
+        parser.add_argument('--cmd', default=None,
+                            help="Command to run in the Docker container")
+        parser.add_argument('--pull', default=False, action='store_true',
+                            help="Whether to pull the image of the project")
+        parser.add_argument('--force-pull', default=False, action='store_true',
+                            help="Whether to force pull the image of the project")
+        parser.add_argument('--build', default=False, action='store_true',
+                            help="Whether to build the image of the project")
+        parser.add_argument('--no-multiarch', default=False, action='store_true',
+                            help="Whether to disable multiarch support (based on bin_fmt)")
+        parser.add_argument('-f', '--force', default=False, action='store_true',
+                            help="Whether to force the run when the git index is not clean")
+        parser.add_argument('-M', '--mount', default=False, action='store_true',
+                            help="Whether to mount the current project into the container")
+        parser.add_argument('-u','--username',default="duckietown",
+                            help="The docker registry username that owns the Docker image")
+        parser.add_argument('--rm', default=True, action='store_true',
+                            help="Whether to remove the container once done")
+        parser.add_argument('docker_args', nargs='*', default=[])
+        parsed, _ = parser.parse_known_args(args=args)
+        # ---
+        dtslogger.info('Project workspace: {}'.format(parsed.workdir))
+        # show info about project
+        shell.include.devel.info.command(shell, args)
+        # get info about current repo
+        repo_info = shell.include.devel.info.get_repo_info(parsed.workdir)
+        repo = repo_info['REPOSITORY']
+        branch = repo_info['BRANCH']
+        nmodified = repo_info['INDEX_NUM_MODIFIED']
+        nadded = repo_info['INDEX_NUM_ADDED']
+        # check if the index is clean
+        if parsed.mount and nmodified + nadded > 0:
+            dtslogger.warning('Your index is not clean (some files are not committed).')
+            dtslogger.warning('If you know what you are doing, use --force (-f) to force the execution of the command.')
+            if not parsed.force:
+                exit(1)
+            dtslogger.warning('Forced!')
+        # create image name
+        image = "%s/%s:%s-%s" % (parsed.username, repo, branch, parsed.arch)
+        # get info about docker endpoint
+        dtslogger.info('Retrieving info about Docker endpoint...')
+        epoint = _run_cmd([
+            'docker',
+                '-H=%s' % parsed.machine,
+                'info',
+                    '--format',
+                    '{{json .}}'
+        ], get_output=True, print_output=False)
+        epoint = json.loads(epoint)
+        if 'ServerErrors' in epoint:
+            dtslogger.error('\n'.join(epoint['ServerErrors']))
+            return
+        epoint['MemTotal'] = _sizeof_fmt(epoint['MemTotal'])
+        print(DOCKER_INFO.format(**epoint))
+        # print info about multiarch
+        msg = 'Running an image for {} on {}.'.format(parsed.arch, epoint['Architecture'])
+        dtslogger.info(msg)
+        # register bin_fmt in the target machine (if needed)
+        if not parsed.no_multiarch:
+            compatible_archs = ARCH_COMPATIBILITY_MAP[CANONICAL_ARCH[epoint['Architecture']]]
+            if parsed.arch not in compatible_archs:
+                dtslogger.info('Configuring machine for multiarch...')
+                try:
+                    _run_cmd([
+                        'docker',
+                            '-H=%s' % parsed.machine,
+                            'run',
+                                '--rm',
+                                '--privileged',
+                                'multiarch/qemu-user-static:register',
+                                '--reset'
+                    ], True)
+                    dtslogger.info('Multiarch Enabled!')
+                except:
+                    msg = 'Multiarch cannot be enabled on the target machine. This might create issues.'
+                    dtslogger.warning(msg)
+            else:
+                msg = 'Running an image for {} on {}. Multiarch not needed!'.format(parsed.arch, epoint['Architecture'])
+                dtslogger.info(msg)
+        # pulling image (if requested)
+        if parsed.pull or parsed.force_pull:
+            # check if the endpoint contains an image with the same name
+            is_present = False
+            try:
+                out = _run_cmd([
+                    'docker',
+                        '-H=%s' % parsed.machine,
+                        'images',
+                            '--format',
+                            "{{.Repository}}:{{.Tag}}"
+                ], get_output=True, print_output=False, suppress_errors=True)
+                is_present = image in out
+            except:
+                pass
+            if not is_present or parsed.force_pull:
+                # try to pull the image
+                dtslogger.info('Pulling image "%s"...' % image)
+                try:
+                    _run_cmd([
+                        'docker',
+                            '-H=%s' % parsed.machine,
+                            'pull',
+                                image
+                    ], get_output=True, print_output=True, suppress_errors=True)
+                except:
+                    dtslogger.warning('An error occurred while pulling the image "%s", maybe the image does not exist' % image)
+            else:
+                dtslogger.info('Found an image with the same name. Using it. User --force-pull to force a new pull.')
+        # cmd option
+        cmd_option = [] if not parsed.cmd else [parsed.cmd]
+        # mount option
+        mount_option = []
+        if parsed.mount:
+            mount_option = [
+                '-v', '{:s}:{:s}'.format(
+                    os.path.join(parsed.workdir, 'packages'),
+                    '/code/{:s}/'.format(repo)
+                ),
+                '-v', '{:s}:{:s}'.format(
+                    os.path.join(parsed.workdir, 'launch.sh'),
+                    '/launch/{:s}/launch.sh'.format(repo)
+                )
+            ]
+        # docker arguments
+        if not parsed.docker_args:
+            parsed.docker_args = []
+        if parsed.rm:
+            parsed.docker_args += ['--rm']
+        # container name
+        if not parsed.name:
+            parsed.name = 'dts-run-{:s}'.format(repo)
+        parsed.docker_args += ['--name', parsed.name]
+        # escape spaces in arguments
+        parsed.docker_args = [a.replace(' ', '\\ ') for a in parsed.docker_args]
+        # run
+        _run_cmd([
+            'docker',
+                '-H=%s' % parsed.machine,
+                'run', '-it'] +
+                    parsed.docker_args +
+                    mount_option +
+                    [image] +
+                    cmd_option
+        )
+
+
+    @staticmethod
+    def complete(shell, word, line):
+        return []
+
+
+
+def _run_cmd(cmd, get_output=False, print_output=False, suppress_errors=False, shell=False):
+    if shell:
+        cmd = ' '.join([str(s) for s in cmd])
+    dtslogger.debug('$ %s' % cmd)
+    if get_output:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=shell)
+        proc.wait()
+        if proc.returncode != 0:
+            if not suppress_errors:
+                msg = 'The command {} returned exit code {}'.format(cmd, proc.returncode)
+                dtslogger.error(msg)
+            raise RuntimeError(msg)
+        out = proc.stdout.read().decode('utf-8').rstrip()
+        if print_output:
+            print(out)
+        return out
+    else:
+        subprocess.check_call(cmd, shell=shell)
+
+
+def _sizeof_fmt(num, suffix='B'):
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.2f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.2f%s%s" % (num, 'Yi', suffix)
