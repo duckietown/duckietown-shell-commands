11331a82e01df5389aa0b7d12b141c14dcc822f1
Andrea F. Daniele <afdaniele@ttic.edu>
2020-09-05 04:27:42 -0500

    minor

--- update/__init__.py
+++ update/__init__.py
@@ -40,7 +40,7 @@ Detected Duckietown Shell %s but these commands (%s) need Duckietown Shell >= %s
 Please, update your Duckietown Shell using the following command,
 \n\n
         pip3 install -U duckietown-shell
-\n\n\n
+\n\n.
 """ % (
             render_version(vnow),
             duckietown_shell_commands_version,
