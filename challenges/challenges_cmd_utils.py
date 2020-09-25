3e445b42b060566b65ad5118f4528e44836994f5
Andrea Censi <acensi@ethz.ch>
2020-09-25 21:46:07 +0200

    dep

--- challenges/challenges_cmd_utils.py
+++ challenges/challenges_cmd_utils.py
@@ -12,7 +12,7 @@ def v(x):
 
 def check_duckietown_challenges_version():
     PKG = "duckietown-challenges-daffy"
-    required = (6, 0, 22)
+    required = (6, 0, 24)
 
     PKG_VERSION = v(required)
 
