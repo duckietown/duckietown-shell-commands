6493f6309c21ebe0833e48ddbc7eeaa1dae93a9a
Andrea Censi <acensi@ethz.ch>
2020-09-28 20:27:39 +0200

    fix

--- update/__init__.py
+++ update/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 6):
     msg = "duckietown-shell-commands requires Python 3.6 and later.\nDetected %s." % str(sys.version)
     raise UserError(msg)
 
-min_duckietown_shell = ".".join(["5", "1", "7"])
+min_duckietown_shell = ".".join(["5", "1", "16"])
 duckietown_shell_commands_version = "5.1.0"
 
 
