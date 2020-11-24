397cd3906ca9eb0aa4339e1ad8bbff8ddbb1f370
Liam Paull <paulll@iro.umontreal.ca>
2020-11-24 17:59:01 -0500

    lower case

--- utils/exercise_utils.py
+++ utils/exercise_utils.py
@@ -1,5 +1,5 @@
 BASELINE_IMAGES = {
     "template_ros": "duckietown/challenge-aido_lf-template-ros:daffy",
     "duckietown_baseline": "duckietown/challenge-aido_lf-baseline-duckietown:daffy",
-    "RPL_ros_baseline": "duckietown/challenge-aido_lf-basline-rpl-ros:daffy",
+    "rpl_ros_baseline": "duckietown/challenge-aido_lf-basline-rpl-ros:daffy",
 }
