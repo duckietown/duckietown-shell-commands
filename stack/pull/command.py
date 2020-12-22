4bf78e2c39b6ec70add8381abd48cd6e4683b5ff
Andrea F. Daniele <afdaniele@ttic.edu>
2020-12-22 16:08:08 -0600

    stack/pull: added command

--- stack/pull/command.py
+++ stack/pull/command.py
@@ -5,20 +5,20 @@ import argparse
 import yaml
 
 from utils.avahi_utils import wait_for_service
-from utils.cli_utils import start_command_in_subprocess
 
 from dt_shell import DTCommandAbs, dtslogger, DTShell
 from utils.docker_utils import get_endpoint_architecture, pull_image
 from utils.misc_utils import sanitize_hostname
-from utils.docker_utils import DEFAULT_MACHINE, DEFAULT_DOCKER_TCP_PORT
+from utils.docker_utils import DEFAULT_MACHINE
 from utils.multi_command_utils import MultiCommand
 
 DEFAULT_STACK = 'default'
+DUCKIETOWN_STACK = 'duckietown'
 
 
 class DTCommand(DTCommandAbs):
 
-    help = "Easy way to run code on Duckietown robots"
+    help = "Easy way to pull code on Duckietown robots"
 
     @staticmethod
     def command(shell: DTShell, args):
@@ -30,21 +30,7 @@ class DTCommand(DTCommandAbs):
             default=None,
             help="Docker socket or hostname where to run the image",
         )
-        parser.add_argument(
-            "-d",
-            "--detach",
-            action='store_true',
-            default=False,
-            help="Detach from running containers",
-        )
-        parser.add_argument(
-            "--pull",
-            action='store_true',
-            default=False,
-            help="Pull images before running",
-        )
         parser.add_argument("stack", nargs=1, default=None)
-        parser.add_argument("docker_args", nargs="*", default=[])
         parsed, _ = parser.parse_known_args(args=args)
         # ---
         # try to interpret it as a multi-command
@@ -54,17 +40,15 @@ class DTCommand(DTCommandAbs):
             return
         # ---
         parsed.stack = parsed.stack[0]
-        project_name = parsed.stack.replace('/', '_')
         # special stack is `duckietown`
-        if parsed.stack == 'duckietown':
+        if parsed.stack == DUCKIETOWN_STACK:
             # retrieve robot type from device
             dtslogger.info(f'Waiting for device "{parsed.machine}"...')
             hostname = parsed.machine.replace(".local", "")
             _, _, data = wait_for_service("DT::ROBOT_TYPE", hostname)
             rtype = data['type']
             dtslogger.info(f'Detected device type is "{rtype}".')
-            parsed.stack = f'duckietown/{rtype}'
-            project_name = 'duckietown'
+            parsed.stack = f'{DUCKIETOWN_STACK}/{rtype}'
         # sanitize stack
         stack = parsed.stack if '/' in parsed.stack else f"{parsed.stack}/{DEFAULT_STACK}"
         # check stack
@@ -82,39 +66,15 @@ class DTCommand(DTCommandAbs):
         dtslogger.info("Retrieving info about Docker endpoint...")
         endpoint_arch = get_endpoint_architecture(parsed.machine)
         dtslogger.info(f'Detected device architecture is "{endpoint_arch}".')
-        # pull images
-        if parsed.pull:
-            with open(stack_file, 'r') as fin:
-                stack_content = yaml.safe_load(fin)
-            for service in stack_content['services'].values():
-                image_name = service['image'].replace('${ARCH}', endpoint_arch)
-                dtslogger.info(f"Pulling image `{image_name}`...")
-                pull_image(image_name, parsed.machine)
         # print info
-        dtslogger.info(f"Running stack [{stack}]...")
+        dtslogger.info(f"Pulling stack [{stack}]...")
         print("------>")
-        # collect arguments
-        docker_arguments = []
-        # get copy of environment
-        env = {}
-        env.update(os.environ)
-        # add ARCH
-        env['ARCH'] = endpoint_arch
-        # -d/--detach
-        if parsed.detach:
-            docker_arguments.append("--detach")
-        # run docker compose stack
-        H = f"{parsed.machine}:{DEFAULT_DOCKER_TCP_PORT}"
-        start_command_in_subprocess(
-            [
-                'docker-compose',
-                f"-H={H}",
-                "--project-name", project_name,
-                "--file", stack_file,
-                "up"
-            ]
-            + docker_arguments,
-            env=env
-        )
+        # pull images
+        with open(stack_file, 'r') as fin:
+            stack_content = yaml.safe_load(fin)
+        for service in stack_content['services'].values():
+            image_name = service['image'].replace('${ARCH}', endpoint_arch)
+            dtslogger.info(f"Pulling image `{image_name}`...")
+            pull_image(image_name, parsed.machine)
         # ---
         print("<------")
