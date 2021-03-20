b7d41f9bb69f5caa03cfe5eee668a1783e8d7b0d
Andrea F. Daniele <afdaniele@ttic.edu>
2021-03-20 01:28:54 -0500

    duckiebot/update: added list of static images to update

--- duckiebot/update/command.py
+++ duckiebot/update/command.py
@@ -1,8 +1,15 @@
 import argparse
 
-from dt_shell import DTCommandAbs, DTShell
+from dt_shell import DTCommandAbs, DTShell, dtslogger
+from utils.docker_utils import get_endpoint_architecture, get_client, pull_image
+from utils.duckietown_utils import get_distro_version
+from utils.misc_utils import sanitize_hostname
 
 DEFAULT_STACK = "duckietown"
+OTHER_IMAGES_TO_UPDATE = [
+    "duckietown/dt-gui-tools:{distro}-{arch}",
+    "duckietown/dt-core:{distro}-{arch}",
+]
 
 
 class DTCommand(DTCommandAbs):
@@ -28,6 +35,17 @@ class DTCommand(DTCommandAbs):
         parsed = parser.parse_args(args)
         # sanitize arguments
         parsed.robot = parsed.robot[0]
+        hostname = sanitize_hostname(parsed.robot)
+        # compile image names
+        arch = get_endpoint_architecture(hostname)
+        distro = get_distro_version(shell)
+        images = [
+            img.format(distro=distro, arch=arch) for img in OTHER_IMAGES_TO_UPDATE
+        ]
+        client = get_client(hostname)
+        for image in images:
+            dtslogger.info(f"Pulling image `{image}`...")
+            pull_image(image, client)
         # call `stack up` command
         shell.include.stack.up.command(shell, [
             "--machine", parsed.robot,
