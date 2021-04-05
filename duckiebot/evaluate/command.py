c3aa336aa0feafd408482f773a2c53408f6044a9
Andrea F. Daniele <afdaniele@ttic.edu>
2021-04-05 11:41:17 -0500

    duckiebot/evaluate: removed --nocalib workaround to a bug

--- duckiebot/evaluate/command.py
+++ duckiebot/evaluate/command.py
@@ -67,12 +67,6 @@ class DTCommand(DTCommandAbs):
             help="If true record a rosbag",
         )
         group.add_argument(
-            "--nocalib",
-            action="store_true",
-            default=False,
-            help="Ignore calibration files",
-        )
-        group.add_argument(
             "--debug",
             action="store_true",
             default=False,
@@ -120,10 +114,7 @@ class DTCommand(DTCommandAbs):
             try:
                 get_calibration_files(dir_fake_home, parsed.duckiebot_name)
             except CannotGetCalibration as e:
-                if parsed.nocalib:
-                    dtslogger.warn(e)
-                else:
-                    raise
+                raise
 
         duckiebot_ip = get_duckiebot_ip(duckiebot_name)
         if parsed.raspberrypi:
