af891934c1df01e1b687f93a3c02c9909c21e837
Andrea F. Daniele <afdaniele@ttic.edu>
2021-04-05 23:18:21 -0500

    exercises/notebooks: refactored to use Jupyter notebook from dt-gui-tools

--- exercises/notebooks/command.py
+++ exercises/notebooks/command.py
@@ -1,8 +1,10 @@
 import os
+import time
+import webbrowser
+from pathlib import Path
+from threading import Thread
 
-from dt_shell import DTCommandAbs, DTShell, UserError
-
-from utils.cli_utils import start_command_in_subprocess
+from dt_shell import DTCommandAbs, DTShell, UserError, dtslogger
 
 usage = """
 
@@ -12,14 +14,12 @@ usage = """
 
     To know more on the `exercise` commands, use `dts duckiebot exercise -h`.
 
-        $ dts exercise notebooks 
+        $ dts exercises notebooks 
 
 """
 
-BRANCH = "daffy"
-ARCH = "amd64"
-# AIDO_REGISTRY = "registry-stage.duckietown.org"
-ROS_TEMPLATE_IMAGE = "duckietown/challenge-aido_lf-template-ros:" + BRANCH + "-" + ARCH
+JUPYTER_WS = "/jupyter_ws"
+JUPYTER_URL = "http://localhost:8888"
 
 
 class InvalidUserInput(UserError):
@@ -29,11 +29,41 @@ class InvalidUserInput(UserError):
 class DTCommand(DTCommandAbs):
     @staticmethod
     def command(shell: DTShell, args):
-        prog = "dts exercise notebooks"
         working_dir = os.getcwd()
-        CF = "config.yaml"
-        fn = os.path.join(working_dir, CF)
-        if not os.path.exists(fn):
-            msg = f"You must run this command inside an exercise directory containing a `{CF}` file."
+        exercise_name = Path(working_dir).stem
+        dtslogger.info(f"Exercise name: {exercise_name}")
+        # make sure we are in an exercise directory
+        cfile_name = "config.yaml"
+        cfile = os.path.join(working_dir, cfile_name)
+        if not os.path.exists(cfile):
+            msg = f"You must run this command inside an exercise directory " \
+                  f"containing a `{cfile_name}` file."
             raise InvalidUserInput(msg)
-        start_command_in_subprocess("cd notebooks && jupyter notebook")
+        # make sure this exercise has a notebooks directory in it
+        notesdir_name = "notebooks"
+        notesdir = os.path.join(working_dir, notesdir_name)
+        if not os.path.exists(notesdir) or not os.path.isdir(notesdir):
+            msg = f"You must run this command inside an exercise directory " \
+                  f"containing a `{notesdir_name}` directory."
+            raise InvalidUserInput(msg)
+
+        # create a function that opens up the browser to the right URL after 4 seconds
+        def open_url():
+            # wait 4 seconds, then open the browser
+            time.sleep(4)
+            dtslogger.info(f"Open your browser at the following address to use "
+                           f"your notebooks: {JUPYTER_URL}\n\n\n")
+            webbrowser.open(JUPYTER_URL)
+
+        waiter = Thread(target=open_url)
+        waiter.start()
+
+        # run start-gui-tools
+        shell.include.start_gui_tools.command(shell, [
+            "--launcher", "jupyter",
+            "--mount", f"{notesdir}:{JUPYTER_WS}",
+            "--name", f"dts-exercises-notebooks-{exercise_name}",
+            "--no-scream"
+        ])
+
+        waiter.join()
