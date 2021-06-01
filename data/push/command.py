676e935f801d1edbc3cda5f9528e0f9bd4d6ebbf
Andrea F. Daniele <afdaniele@ttic.edu>
2021-06-01 17:41:08 -0500

    data/push: bug fix

--- data/push/command.py
+++ data/push/command.py
@@ -108,7 +108,6 @@ Where <space> can be one of {str(VALID_SPACES)}.
         else:
             # the user provided a token, use that one
             token = parsed.token
-            dtslogger.debug(f"Using custom token: {token[:-4]}XXXX")
         # create storage client
         client = DataClient(token)
         storage = client.storage(parsed.space)
