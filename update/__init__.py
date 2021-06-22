56046e35ba182bbeae28a7d7f20782b241e2a322
Andrea Censi <acensi@ethz.ch>
2021-06-22 13:22:28 +0200

    fix version

--- update/__init__.py
+++ update/__init__.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 6):
     raise UserError(msg)
 
 min_duckietown_shell = ".".join(["5", "2", "8"])
-duckietown_shell_commands_version = "5.3.20210414"
+duckietown_shell_commands_version = "5.4.0"
 
 
 def parse_version(x):
