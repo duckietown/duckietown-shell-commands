9ea70537ebdcd9a2cded980cc62679be74a2d386
Andrea F. Daniele <afdaniele@ttic.edu>
2021-07-29 10:40:49 -0500

    added support for Duckiedrone DD21

--- utils/duckietown_utils.py
+++ utils/duckietown_utils.py
@@ -19,7 +19,7 @@ def get_robot_types():
 def get_robot_configurations(robot_type):
     configurations = {
         "duckiebot": ["DB18", "DB19", "DB20", "DB21M", "DB21J", "DB21R"],
-        "duckiedrone": ["DD18"],
+        "duckiedrone": ["DD18", "DD21"],
         "watchtower": ["WT18", "WT19A", "WT19B", "WT21A", "WT21B"],
         "greenstation": ["GS17"],
         "workstation": ["WS21A", "WS21B", "WS21C"],
@@ -46,7 +46,7 @@ def get_robot_hardware(robot_configuration):
         "DB21R": ("raspberry_pi_64", "4B"),
         # Duckiedrone
         "DD18": ("raspberry_pi", "3B"),
-        "DD21": ("raspberry_pi", "3B+"),
+        "DD21": ("raspberry_pi_64", "3B+"),
         # Watchtower
         "WT18": ("raspberry_pi", "4B2G"),
         "WT19A": ("raspberry_pi", "4B2G"),
