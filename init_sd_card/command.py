e15566c6f4bb9230229ea1c14cb07fac9ee5e283
Andrea F. Daniele <afdaniele@ttic.edu>
2021-09-07 15:28:37 -0500

    bumping DB21J stable image to v1.2.2

--- init_sd_card/command.py
+++ init_sd_card/command.py
@@ -56,7 +56,7 @@ def DISK_IMAGE_VERSION(robot_configuration, experimental=False):
     board_to_disk_image_version = {
         "raspberry_pi": {"stable": "1.2.1", "experimental": "1.2.1"},
         "raspberry_pi_64": {"stable": "2.0.0", "experimental": "2.0.0"},
-        "jetson_nano_4gb": {"stable": "1.2.0", "experimental": "1.2.2"},
+        "jetson_nano_4gb": {"stable": "1.2.2", "experimental": "1.2.2"},
         "jetson_nano_2gb": {"stable": "1.2.2", "experimental": "1.2.2"},
     }
     board, _ = get_robot_hardware(robot_configuration)
