bd21159793b066226804b4bdb23675c33226f549
Andrea F. Daniele <afdaniele@ttic.edu>
2021-11-01 10:45:22 -0500

    Revert "minor"
    This reverts commit 01fa48721026cf193662d05c0ac4ff3fe152d04e.

--- utils/docker_utils.py
+++ utils/docker_utils.py
@@ -195,10 +195,10 @@ def _login_client(client: DockerClient, registry: str, username: str, password: 
     """Raises CouldNotLogin"""
     password_hidden = hide_string(password)
     dtslogger.info(f"Logging in to {registry} as {username!r} with secret {password_hidden!r}`")
-    res = client.login(username=username, password=password, registry=registry)
+    res = client.login(username=username, password=password, registry=registry, reauth=True)
     dtslogger.debug(f"login response: {res}")
     # Status': 'Login Succeeded'
-    if res.get("Status", None) in [None, "Login Succeeded"]:
+    if res.get("Status", None) == "Login Succeeded":
         pass
     else:
         if raise_on_error:
