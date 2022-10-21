dd311b660d0f7c8ff016915dc2fbd2b326cb4c38
Andrea F. Daniele <afdaniele@ttic.edu>
2022-10-21 01:58:18 -0400

    setup/mkcert: bug fixes

--- setup/mkcert/command.py
+++ setup/mkcert/command.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import os
 import platform
 import stat
@@ -7,6 +8,8 @@ import subprocess
 from pathlib import Path
 from typing import List
 from subprocess import STDOUT
+
+from os.path import join, exists, expanduser
 
 import requests
 from dt_shell import DTCommandAbs, DTShell, dtslogger
@@ -33,9 +36,9 @@ class DTCommand(DTCommandAbs):
         )
         parsed = parser.parse_args(args=args)
         # define location for SSL certificate and key
-        root: str = os.path.expanduser(DTShellConstants.ROOT)
-        ca_dir: str = os.path.join(root, "secrets", "mkcert", "ca")
-        ssl_dir: str = os.path.join(root, "secrets", "mkcert", "ssl")
+        root: str = expanduser(DTShellConstants.ROOT)
+        ca_dir: str = join(root, "secrets", "mkcert", "ca")
+        ssl_dir: str = join(root, "secrets", "mkcert", "ssl")
         cmd_env = {"CAROOT": ca_dir}
         env = {**os.environ, **cmd_env}
 
@@ -55,9 +58,10 @@ class DTCommand(DTCommandAbs):
         # - make sure the directory exists
         os.makedirs(ca_dir, exist_ok=True)
         # - define CA files
-        ca_cert: str = os.path.join(ca_dir, "rootCA.pem")
-        ca_key: str = os.path.join(ca_dir, "rootCA-key.pem")
-        ca_exists: bool = os.path.exists(ca_cert) and os.path.exists(ca_key)
+        ca_cert: str = join(ca_dir, "rootCA.pem")
+        ca_key: str = join(ca_dir, "rootCA-key.pem")
+        ca_flag: str = join(ca_dir, "rootCA-key.installed")
+        ca_exists: bool = exists(ca_flag) and exists(ca_cert) and exists(ca_key)
 
         # - make certificate authority and install
         if not ca_exists:
@@ -66,17 +70,25 @@ class DTCommand(DTCommandAbs):
             cmd: List[str] = DTCommand._mkcert_command("-install")
             dtslogger.debug(f"Running command:\n\t$ {cmd}\n\tenv: {cmd_env}\n")
             out = subprocess.check_output(cmd, env=env, stderr=STDOUT).decode("utf-8")
-            print(out)
             # make sure the CA was created
-            if "Created a new local CA" not in out:
+            if "Created a new local CA" not in out and "The local CA is already installed" not in out:
                 raise Exception(f"An error occurred while creating a local CA:\n\n{out}")
-            assert os.path.exists(ca_cert)
-            assert os.path.exists(ca_key)
+            assert exists(ca_cert)
+            assert exists(ca_key)
+            # look for missing libraries
+            if "libnss3-tools" in out:
+                dtslogger.error("The system library 'libnss3-tools' is missing, please, "
+                                  "install it using the following command and the retry:\n\n"
+                                  "\t$ sudo apt install libnss3-tools\n\n")
+                exit(1)
+            print(out)
             # make sure the CA was installed
             installed: bool = "the local CA is not installed" not in out
             if not installed:
                 raise Exception(f"An error occurred while installing the local CA:\n\n{out}")
             # ---
+            with open(ca_flag, "wt") as fout:
+                fout.write(str(datetime.datetime.now().isoformat()))
             dtslogger.info("A new local Certificate Authority was successfully installed.")
         else:
             dtslogger.info(f"Existing local Certificate Authority found in [{ca_dir}]")
@@ -85,9 +97,9 @@ class DTCommand(DTCommandAbs):
         # - make sure the directory exists
         os.makedirs(ssl_dir, exist_ok=True)
         # - make domain certificate
-        ssl_cert: str = os.path.join(ssl_dir, f"{LOCAL_DOMAIN}.pem")
-        ssl_key: str = os.path.join(ssl_dir, f"{LOCAL_DOMAIN}-key.pem")
-        ssl_exists: bool = os.path.exists(ssl_cert) and os.path.exists(ssl_key)
+        ssl_cert: str = join(ssl_dir, f"{LOCAL_DOMAIN}.pem")
+        ssl_key: str = join(ssl_dir, f"{LOCAL_DOMAIN}-key.pem")
+        ssl_exists: bool = exists(ssl_cert) and exists(ssl_key)
 
         # - make domain certificate
         if not ssl_exists:
@@ -101,8 +113,8 @@ class DTCommand(DTCommandAbs):
             # make sure the domain certificate was created
             if "Created a new certificate valid for the following names" not in out:
                 raise Exception(f"An error occurred while creating a domain certificate:\n\n{out}")
-            assert os.path.exists(ssl_cert)
-            assert os.path.exists(ssl_key)
+            assert exists(ssl_cert)
+            assert exists(ssl_key)
             # ---
             dtslogger.info(f"A new certificate for the domain '{LOCAL_DOMAIN}' was created.")
         else:
@@ -132,12 +144,12 @@ class DTCommand(DTCommandAbs):
     @staticmethod
     def _install_mkcert():
         # make bin directory
-        root: str = os.path.expanduser(DTShellConstants.ROOT)
-        bin_dir: str = os.path.join(root, "bin")
+        root: str = expanduser(DTShellConstants.ROOT)
+        bin_dir: str = join(root, "bin")
         os.makedirs(bin_dir, exist_ok=True)
         # do nothing if mkcert can be found locally
-        bin: str = os.path.join(bin_dir, "mkcert")
-        if os.path.exists(bin):
+        bin: str = join(bin_dir, "mkcert")
+        if exists(bin):
             dtslogger.debug(f"Binary for `mkcert` found at '{bin}'")
             return
         # download mkcert binary
@@ -154,10 +166,10 @@ class DTCommand(DTCommandAbs):
 
     @staticmethod
     def _mkcert_command(*args) -> List[str]:
-        root: str = os.path.expanduser(DTShellConstants.ROOT)
-        bin_dir: str = os.path.join(root, "bin")
-        bin: str = os.path.join(bin_dir, "mkcert")
-        assert os.path.exists(bin)
+        root: str = expanduser(DTShellConstants.ROOT)
+        bin_dir: str = join(root, "bin")
+        bin: str = join(bin_dir, "mkcert")
+        assert exists(bin)
         return [bin, *args]
 
     @staticmethod
