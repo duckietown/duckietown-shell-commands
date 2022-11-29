dfe1e027fa49ba5e7ec0a874d4049f4a2d881d58
liampaull <paulll@iro.umontreal.ca>
2022-11-29 15:41:40 -0500

    don't mount when in simulation

--- code/workbench/command.py
+++ code/workbench/command.py
@@ -135,8 +135,7 @@ class SettingsFile:
 
     def __str__(self):
         fields: Iterable[dataclasses.Field] = dataclasses.fields(SettingsFile)
-        return "\n\t" + \
-               "\n\t".join(f"{field.name}: {getattr(self, field.name)}" for field in fields) + "\n"
+        return "\n\t" + "\n\t".join(f"{field.name}: {getattr(self, field.name)}" for field in fields) + "\n"
 
 
 ALLOWED_LEVELS = [e.value for e in Levels]
@@ -156,10 +155,7 @@ class DTCommand(DTCommandAbs):
         parser = argparse.ArgumentParser(prog=prog, usage=usage)
 
         parser.add_argument(
-            "-C",
-            "--workdir",
-            default=os.getcwd(),
-            help="Directory containing the project to bring up"
+            "-C", "--workdir", default=os.getcwd(), help="Directory containing the project to bring up"
         )
 
         parser.add_argument(
@@ -206,11 +202,7 @@ class DTCommand(DTCommandAbs):
         )
 
         parser.add_argument(
-            "--pull",
-            dest="pull",
-            action="store_true",
-            default=False,
-            help="Should we pull all of the images"
+            "--pull", dest="pull", action="store_true", default=False, help="Should we pull all of the images"
         )
 
         loglevels_friendly = " ".join(f"{k.value}:{v}" for k, v in LOG_LEVELS.items())
@@ -426,11 +418,7 @@ class DTCommand(DTCommandAbs):
             if token is None:
                 raise UserError("Please set token using the command 'dts tok set'")
             # get container specs from the challenges server
-            images = get_challenge_images(
-                challenge=challenge,
-                step=settings.step,
-                token=token
-            )
+            images = get_challenge_images(challenge=challenge, step=settings.step, token=token)
             sim_spec = images["simulator"]
             expman_spec = images["evaluator"]
         else:
@@ -531,8 +519,9 @@ class DTCommand(DTCommandAbs):
                     pull_if_not_exist(agent_client, image)
                 except NotFound:
                     if image == agent_image:
-                        dtslogger.error("Run 'dts code build' to build your agent before running "
-                                        "the workbench.")
+                        dtslogger.error(
+                            "Run 'dts code build' to build your agent before running " "the workbench."
+                        )
                         exit(1)
 
         # create a docker network to deploy the containers in
@@ -903,7 +892,7 @@ class DTCommand(DTCommandAbs):
 
         # find the port the OS assigned to the container, then print it in 6 seconds
         vnc_container.reload()
-        port: str = vnc_container.attrs['NetworkSettings']['Ports']['8087/tcp'][0]['HostPort']
+        port: str = vnc_container.attrs["NetworkSettings"]["Ports"]["8087/tcp"][0]["HostPort"]
 
         def print_nvc_port_later():
             time.sleep(6)
@@ -1143,18 +1132,19 @@ def launch_agent(
     if agent_is_remote or os.path.exists(local_launch):
         agent_volumes[local_launch] = {"bind": destination_launch, "mode": "rw"}
 
-    # define the location of the /data/config to give to the agent
-    data = os.path.join(recipe.path, "assets", "agent", "data") if agent_is_local else "/data"
+    if not parsed.simulation:
+        # define the location of the /data/config to give to the agent
+        data = os.path.join(recipe.path, "assets", "agent", "data") if agent_is_local else "/data"
 
-    # get the calibrations from the robot with the REST API
-    if parsed.local and not parsed.simulation:
-        data = os.path.join(tmpdir, "agent", "data")
-        os.makedirs(data)
-        # copy /data/config from the robot to a temporary location
-        get_calibration_files(data, parsed.duckiebot)
+        # get the calibrations from the robot with the REST API
+        if parsed.local:
+            data = os.path.join(tmpdir, "agent", "data")
+            os.makedirs(data)
+            # copy /data/config from the robot to a temporary location
+            get_calibration_files(data, parsed.duckiebot)
 
-    # add agent's configuration
-    agent_volumes[os.path.join(data, "config")] = {"bind": "/data/config", "mode": "rw"}
+        # add agent's configuration
+        agent_volumes[os.path.join(data, "config")] = {"bind": "/data/config", "mode": "rw"}
 
     # add user to groups if on linux
     on_mac = "Darwin" in platform.system()
@@ -1337,8 +1327,7 @@ def get_challenge_from_submission_file(recipe: DTProject) -> str:
     return submission["challenge"][0]
 
 
-def get_challenge_images(challenge: str, step: Optional[str], token: str) -> \
-        Dict[str, ImageRunSpec]:
+def get_challenge_images(challenge: str, step: Optional[str], token: str) -> Dict[str, ImageRunSpec]:
     default = "https://challenges.duckietown.org/v4"
     server = os.environ.get("DTSERVER", default)
     url = f"{server}/api/challenges/{challenge}/description"
