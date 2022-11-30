7e595be5dbe399e8fc3fd68c7849f444cf5787fb
Andrea F. Daniele <afdaniele@duckietown.com>
2022-11-30 01:42:39 -0500

    Cherry-pick from ente, better parsing of stack files

--- disk_image/create/constants.py
+++ disk_image/create/constants.py
@@ -7,13 +7,14 @@ DATA_STORAGE_DISK_IMAGE_DIR = "disk_image"
 DEFAULT_STACK = "duckietown"
 AUTOBOOT_STACKS_DIR = "/data/autoboot/"
 DEFAULT_DEVICE_ARCH = "arm32v7"
+DEFAULT_DOCKER_REGISTRY = "docker.io"
 DOCKER_IMAGE_TEMPLATE = (
-    lambda owner, module, tag=None, version=None, arch=DEFAULT_DEVICE_ARCH: f"{owner}/{module}:"
+    lambda owner, module, tag=None, version=None, arch=DEFAULT_DEVICE_ARCH, registry=DEFAULT_DOCKER_REGISTRY: f"{registry}/{owner}/{module}:"
     + (f"{version}-{arch}" if tag is None else tag)
 )
 
 MODULES_TO_LOAD = [
-    {"owner": "portainer", "module": "portainer", "tag": "linux-arm-1.24.1"},
+    {"owner": "portainer", "module": "portainer", "tag": "1.24.1-alpine"},
     {"owner": "duckietown", "module": "dt-base-environment"},
     {"owner": "duckietown", "module": "dt-commons"},
     {"owner": "duckietown", "module": "dt-device-health"},
