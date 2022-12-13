c1d86ab48ced27cc228b2ef755ab32a116fdce3b
kghite <kghite@gmail.com>
2022-12-13 16:30:05 -0500

    Revert to original function name

--- update/command.py
+++ update/command.py
@@ -12,4 +12,5 @@ class DTCommand(DTCommandAbs):
         if shell.local_commands_info.leave_alone:
             dtslogger.warn("Will not update the commands because the path was set explicitly.")
         else:
-            shell.check_commands_update()
+            shell.update_commands()
+
