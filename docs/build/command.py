8edd8ef1d587e722e6cd9d9b058c6ece84d53376
Andrea F. Daniele <afdaniele@duckietown.com>
2023-01-24 17:26:02 -0500

    docs/*: added --ci option

--- docs/build/command.py
+++ docs/build/command.py
@@ -6,14 +6,15 @@ import os
 import subprocess
 import sys
 from types import SimpleNamespace
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 
+from dt_data_api import DataClient
 from dt_shell import DTCommandAbs, DTShell, dtslogger
 from dt_shell.env_checks import check_docker_environment
 from duckietown_docker_utils import ENV_REGISTRY, replace_important_env_vars
 
-from utils.docker_utils import get_registry_to_use, get_endpoint_architecture
-from utils.dtproject_utils import DTProject
+from utils.docker_utils import get_registry_to_use, get_endpoint_architecture, sanitize_docker_baseurl
+from utils.dtproject_utils import DTProject, get_cloud_builder
 from utils.duckietown_utils import get_distro_version
 from utils.exceptions import ShellNeedsUpdate
 
@@ -24,11 +25,18 @@ except ImportError:
     raise ShellNeedsUpdate("5.4.0+")
 # NOTE: this is to avoid breaking the user workspace
 
+from dockertown import DockerClient
+
 
 CONTAINER_HTML_DIR = "tmp/jb/_build/html"
 CONTAINER_PDF_DIR = "tmp/jb/_build/pdf"
 CONTAINER_BUILD_CACHE_DIR = "/tmp/jb"
 HOST_BUILD_CACHE_DIR = "/tmp/duckietown/docs/{book}"
+
+DCSS_RSA_SECRET_LOCATION = "secrets/rsa/ssh-{dns}/id_rsa"
+DCSS_RSA_SECRET_SPACE = "private"
+SSH_USERNAME = "duckie"
+CLOUD_BUILD_ARCH = "amd64"
 
 
 class DTCommand(DTCommandAbs):
@@ -38,7 +46,10 @@ class DTCommand(DTCommandAbs):
         # Configure args
         parser = argparse.ArgumentParser()
         parser.add_argument(
-            "-C", "--workdir", default=os.getcwd(), help="Directory containing the book to be build"
+            "-C",
+            "--workdir",
+            default=os.getcwd(),
+            help="Directory containing the book to be build"
         )
 
         # Get pre-parsed or parse arguments
@@ -79,6 +90,12 @@ def build_v2(shell: DTShell, args):
         help="Directory containing the book to work on",
     )
     parser.add_argument(
+        "-H",
+        "--machine",
+        default=None,
+        help="Docker socket or hostname where to build the book"
+    )
+    parser.add_argument(
         "--image",
         default=None,
         help="Which environment image to use to build the book. By default, one will be built."
@@ -111,6 +128,12 @@ def build_v2(shell: DTShell, args):
         default=False,
         action="store_true",
         help="Configure the build for CI",
+    )
+    parser.add_argument(
+        "--publish",
+        type=str,
+        default=None,
+        help="Destination hostname of the website to publish, e.g., 'docs.duckietown.com'",
     )
     parser.add_argument(
         "--no-pull",
@@ -147,6 +170,14 @@ def build_v2(shell: DTShell, args):
     if parsed.image and parsed.no_build:
         dtslogger.error("Argument --no-build is implicit when providing a custom environment with --image.")
         exit(1)
+    # -  --machine and not --ci
+    if parsed.machine and not parsed.ci:
+        dtslogger.error("Argument --machine can only be used together with --ci.")
+        exit(1)
+    # -  --ci and not --publish
+    if parsed.ci and parsed.publish is None:
+        dtslogger.error("Argument --ci can only be used together with --publish.")
+        exit(1)
 
     # variables
     registry_to_use = get_registry_to_use()
@@ -157,8 +188,13 @@ def build_v2(shell: DTShell, args):
     parsed.workdir = os.path.abspath(parsed.workdir)
     project: DTProject = DTProject(parsed.workdir)
 
+    # use a cloud machine to build
+    if parsed.ci:
+        parsed.machine = get_cloud_builder(CLOUD_BUILD_ARCH)
+
     # create docker client
-    docker = dockertown.DockerClient(debug=debug)
+    host: Optional[str] = sanitize_docker_baseurl(parsed.machine)
+    docker = DockerClient(host=host, debug=debug)
 
     # pick the right architecture
     dtslogger.info("Retrieving info about Docker endpoint...")
@@ -168,12 +204,10 @@ def build_v2(shell: DTShell, args):
     # find (or build) the book environment image to run
     if parsed.image is not None:
         # custom JB given, just use it
-
         jb_image_name: str = parsed.image
         dtslogger.info(f"Using custom image: {jb_image_name}")
     else:
         # JB environment not provided, we need to build our own (unless --plain)
-
         # custom distro
         if parsed.distro:
             dtslogger.info(f"Using custom distro '{parsed.distro}'")
@@ -193,7 +227,9 @@ def build_v2(shell: DTShell, args):
             if not parsed.no_build:
                 shell.include.docs.env.build.command(shell, args=[], parsed=SimpleNamespace(
                     workdir=parsed.workdir,
+                    machine=parsed.machine,
                     distro=parsed.distro,
+                    embed=parsed.ci,
                     no_pull=parsed.no_pull,
                     verbose=parsed.verbose,
                 ))
@@ -211,25 +247,26 @@ def build_v2(shell: DTShell, args):
 
     # we know which JupyterBook to use
     dtslogger.debug(f"Using JupyterBook image '{jb_image_name}'")
-    source_dir: str = os.path.join(project.path, "src")
-    html_dir: str = os.path.join(project.path, "html")
-    pdf_dir: str = os.path.join(project.path, "pdf")
-    volumes: List[Tuple[str, str, str]] = [
-        # source files
-        (source_dir, "/book", "ro"),
-    ]
 
-    # build HTML
-    build_pdf: bool = parsed.pdf
-    build_html: bool = True
-    if build_html:
-        volumes.append((html_dir, "/out/html", "rw"))
-    # build PDF
-    if build_pdf:
-        volumes.append((pdf_dir, "/out/pdf", "rw"))
-
-    # build cache
     if not parsed.ci:
+        # locations of interest
+        html_dir: str = os.path.join(project.path, "html")
+        pdf_dir: str = os.path.join(project.path, "pdf")
+        volumes: List[Tuple[str, str, str]] = [
+            # source files
+            (project.path, "/book", "ro"),
+        ]
+
+        # build HTML
+        build_pdf: bool = parsed.pdf
+        build_html: bool = True
+        if build_html:
+            volumes.append((html_dir, "/out/html", "rw"))
+        # build PDF
+        if build_pdf:
+            volumes.append((pdf_dir, "/out/pdf", "rw"))
+
+        # build cache
         build_cache: str = HOST_BUILD_CACHE_DIR.format(book=project.name)
         try:
             os.makedirs(build_cache, exist_ok=True)
@@ -238,53 +275,31 @@ def build_v2(shell: DTShell, args):
         if os.path.exists(build_cache):
             volumes.append((build_cache, CONTAINER_BUILD_CACHE_DIR, "rw"))
 
-    # log reader from container
-    def consume_container_logs(_logs):
-        # consume logs
-        for (stream, line) in _logs:
-            line = line.decode("utf-8")
-            if build_html:
-                line = line.replace(CONTAINER_HTML_DIR, html_dir)
-            if build_pdf:
-                line = line.replace(CONTAINER_PDF_DIR, pdf_dir)
-            print(line, end="")
+        # log reader from container
+        def consume_container_logs(_logs):
+            # consume logs
+            for (stream, line) in _logs:
+                line = line.decode("utf-8")
+                if build_html:
+                    line = line.replace(CONTAINER_HTML_DIR, html_dir)
+                if build_pdf:
+                    line = line.replace(CONTAINER_PDF_DIR, pdf_dir)
+                print(line, end="")
 
-    # start the build process
-    dtslogger.info(f"Building project '{project.name}'...")
-    container_name: str = f"docs-build-{project.name}"
-    args = {
-        "image": jb_image_name,
-        "remove": True,
-        "user": f"{os.getuid()}:{os.getuid()}",
-        "envs": {
-            "BOOK_BRANCH_NAME": project.version_name,
-            "DEBUG": "1" if debug else "0"
-        },
-        "volumes": volumes,
-        "name": container_name,
-        "stream": True
-    }
-    dtslogger.debug(
-        f"Calling docker.run with arguments:\n" f"{json.dumps(args, indent=4, sort_keys=True)}\n"
-    )
-    logs = docker.run(**args)
-    consume_container_logs(logs)
-
-    # start the image optimizer process
-    if build_html and (not parsed.no_optimization):
-        dtslogger.info(f"Optimizing media...")
-        container_name: str = f"docs-image-optimizer-{project.name}"
+        # start the book build process
+        dtslogger.info(f"Building project '{project.name}'...")
+        container_name: str = f"docs-build-{project.name}"
         args = {
             "image": jb_image_name,
             "remove": True,
             "user": f"{os.getuid()}:{os.getuid()}",
-            "volumes": volumes,
             "envs": {
-                "DT_LAUNCHER": "jb-optimize-images",
+                "BOOK_BRANCH_NAME": project.version_name,
                 "DEBUG": "1" if debug else "0"
             },
+            "volumes": volumes,
             "name": container_name,
-            "stream": True,
+            "stream": True
         }
         dtslogger.debug(
             f"Calling docker.run with arguments:\n" f"{json.dumps(args, indent=4, sort_keys=True)}\n"
@@ -292,36 +307,105 @@ def build_v2(shell: DTShell, args):
         logs = docker.run(**args)
         consume_container_logs(logs)
 
-    # print HTML location
-    if build_html:
-        loc: str = os.path.abspath(html_dir)
-        bar: str = "=" * len(loc)
-        spc: str = " " * len(loc)
-        dtslogger.info(
-            f"\n\n"
-            f"====================={bar}=====\n"
-            f"|                    {spc}    |\n"
-            f"|    HTML artifacts: {loc}    |\n"
-            f"|                    {spc}    |\n"
-            f"====================={bar}=====\n"
+        # start the image optimizer process
+        if build_html and (not parsed.no_optimization):
+            dtslogger.info(f"Optimizing media...")
+            container_name: str = f"docs-image-optimizer-{project.name}"
+            args = {
+                "image": jb_image_name,
+                "remove": True,
+                "user": f"{os.getuid()}:{os.getuid()}",
+                "volumes": volumes,
+                "envs": {
+                    "DT_LAUNCHER": "jb-optimize-images",
+                    "DEBUG": "1" if debug else "0"
+                },
+                "name": container_name,
+                "stream": True,
+            }
+            dtslogger.debug(
+                f"Calling docker.run with arguments:\n" f"{json.dumps(args, indent=4, sort_keys=True)}\n"
+            )
+            logs = docker.run(**args)
+            consume_container_logs(logs)
+
+        # print HTML location
+        if build_html:
+            loc: str = os.path.abspath(html_dir)
+            bar: str = "=" * len(loc)
+            spc: str = " " * len(loc)
+            dtslogger.info(
+                f"\n\n"
+                f"====================={bar}=====\n"
+                f"|                    {spc}    |\n"
+                f"|    HTML artifacts: {loc}    |\n"
+                f"|                    {spc}    |\n"
+                f"====================={bar}=====\n"
+            )
+
+        # print PDF location
+        if build_pdf:
+            loc: str = os.path.abspath(os.path.join(pdf_dir, "book.pdf"))
+            bar: str = "=" * len(loc)
+            spc: str = " " * len(loc)
+            dtslogger.info(
+                f"\n\n"
+                f"==================={bar}=====\n"
+                f"|                  {spc}    |\n"
+                f"|    PDF artifact: {loc}    |\n"
+                f"|                  {spc}    |\n"
+                f"==================={bar}=====\n"
+            )
+    else:
+        # CI build includes: build HTML, build PDF, image optimization, and artifacts publish
+        dns = parsed.publish
+
+        # download RSA key used to publish artifacts
+        token = os.environ.get("DUCKIETOWN_CI_DT_TOKEN", None)
+        client = DataClient(token)
+        storage = client.storage(DCSS_RSA_SECRET_SPACE)
+        rsa_key_remote = DCSS_RSA_SECRET_LOCATION.format(dns=dns)
+        dtslogger.debug(f"Downloading RSA key from [{DCSS_RSA_SECRET_SPACE}]:{rsa_key_remote}")
+        handler = storage.download(rsa_key_remote)
+        handler.join()
+        dtslogger.info("Download complete!")
+        handler.buffer.seek(0)
+        rsa_key = handler.buffer.read().decode("utf-8")
+
+        # define ssh configuration
+        ssh_hostname = f"ssh-{dns}"
+
+        # log reader from container
+        def consume_container_logs(_logs):
+            # consume logs
+            for (stream, line) in _logs:
+                line = line.decode("utf-8")
+                print(line, end="")
+
+        # start the book build process
+        dtslogger.info(f"Building project '{project.name}'...")
+        args = {
+            "image": jb_image_name,
+            "remove": True,
+            "envs": {
+                "DEBUG": "1",
+                "SSH_KEY": rsa_key,
+                "SSH_HOSTNAME": ssh_hostname,
+                "SSH_USERNAME": SSH_USERNAME,
+                "BOOK_NAME": project.name,
+                "BOOK_BRANCH_NAME": project.version_name,
+                "DT_LAUNCHER": "ci-build"
+            },
+            "stream": True
+        }
+        dtslogger.debug(
+            f"Calling docker.run with arguments:\n" f"{json.dumps(args, indent=4, sort_keys=True)}\n"
         )
-
-    # print PDF location
-    if build_pdf:
-        loc: str = os.path.abspath(os.path.join(pdf_dir, "book.pdf"))
-        bar: str = "=" * len(loc)
-        spc: str = " " * len(loc)
-        dtslogger.info(
-            f"\n\n"
-            f"==================={bar}=====\n"
-            f"|                  {spc}    |\n"
-            f"|    PDF artifact: {loc}    |\n"
-            f"|                  {spc}    |\n"
-            f"==================={bar}=====\n"
-        )
+        logs = docker.run(**args)
+        consume_container_logs(logs)
 
 
-def build_v1(shell: DTShell, args):
+def build_v1(_: DTShell, args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-C",
