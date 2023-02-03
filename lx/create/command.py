67476849852615f42fbea19f07768fff8b057bf5
Andrea F. Daniele <afdaniele@duckietown.com>
2023-02-03 02:14:23 -0500

    lx/create: implemented form

--- lx/create/command.py
+++ lx/create/command.py
@@ -1,10 +1,11 @@
-import argparse
+import json
 import os
-from types import SimpleNamespace
+import pathlib
+from typing import Optional
 
-from dt_shell import DTCommandAbs, dtslogger, DTShell, UserError
-from utils.dtproject_utils import DTProject
-from utils.misc_utils import get_user_login
+from dt_shell import DTCommandAbs, dtslogger, DTShell
+
+from utils.json_schema_form_utils import open_form
 
 
 class DTCommand(DTCommandAbs):
@@ -12,13 +13,21 @@ class DTCommand(DTCommandAbs):
 
     @staticmethod
     def command(shell: DTShell, args, **kwargs):
-        print(
-            'You called the "%s" command, level %d, with arguments %r' % (
-                DTCommand.name,
-                DTCommand.level,
-                args
-            )
+        assets_dir: str = os.path.join(pathlib.Path(__file__).parent.absolute(), "assets", "new-lx")
+        schema_fpath: str = os.path.join(assets_dir, "schema.json")
+        with open(schema_fpath, "rt") as fin:
+            schema: dict = json.load(fin)
+        #
+        values: Optional[dict] = open_form(
+            shell,
+            schema=schema,
+            title="Create new Learning Experience",
+            subtitle="Populate the fields below to create a new Learning Experience",
+            icon=os.path.join(assets_dir, "icon.png"),
         )
+
+        # TODO: do something else with these
+        dtslogger.info(str(values))
 
     @staticmethod
     def complete(shell, word, line):
