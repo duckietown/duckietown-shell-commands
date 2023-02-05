31d49f07d6b60cd8dd19d6e91f2a6f3ff658d16f
kghite <kghite@gmail.com>
2023-02-05 11:15:40 -0500

    Move in branch menu

--- lx/publish/command.py
+++ lx/publish/command.py
@@ -12,16 +12,17 @@ from utils.json_schema_form_utils import open_form
 class DTCommand(DTCommandAbs):
     help = "Publishes a Duckietown Learning Experience to the LX repository"
 
-    # TODO: Initial internal version should update form with dropdown of current duckietown-lx branches
-    print(get_branches("duckietown", "duckietown-lx"))
-
     @staticmethod
     def command(shell: DTShell, args, **kwargs):
         assets_dir: str = os.path.join(pathlib.Path(__file__).parent.absolute(), "assets", "lx-publisher")
         schema_fpath: str = os.path.join(assets_dir, "schema.json")
         with open(schema_fpath, "rt") as fin:
             schema: dict = json.load(fin)
-        #
+
+        # Load duckietown-lx branches into the form
+        # TODO: Loader objects available in schema?
+        schema["schema"]["branch"]["enum"] = get_branches("duckietown", "duckietown-lx")
+
         values: Optional[dict] = open_form(
             shell,
             schema=schema,
