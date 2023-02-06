f9e5f92a33522b590e44c56e5d66c5dfe0ee83da
kghite <kghite@gmail.com>
2023-02-06 18:48:13 -0500

    Update publish form

--- lx/publish/command.py
+++ lx/publish/command.py
@@ -1,14 +1,14 @@
 import argparse
 import json
 import os
-import pathlib
+import tempfile
 from typing import Optional
 
 from dt_shell import DTCommandAbs, dtslogger, DTShell
 
 from utils.dtproject_utils import DTProject
 from utils.git_utils import get_branches
-from utils.json_schema_form_utils import open_form
+from utils.json_schema_form_utils import open_form_from_schema
 
 
 class DTCommand(DTCommandAbs):
@@ -33,26 +33,19 @@ class DTCommand(DTCommandAbs):
         # Ensure this is an LX template
         parsed.workdir = os.path.abspath(parsed.workdir)
         project: DTProject = DTProject(parsed.workdir)
-        if project.type != "template-exercise":
+        if project.type != "development-lx":
             dtslogger.error(f"Project of type '{project.type}' not supported. You need to be in a "
                             f"'template-exercise' project directory to publish with 'dts lx publish'.")
             return False
 
         # Get the form data
-        assets_dir: str = os.path.join(pathlib.Path(__file__).parent.absolute(), "assets", "lx-publisher")
-        schema_fpath: str = os.path.join(assets_dir, "schema.json")
-        with open(schema_fpath, "rt") as fin:
-            schema: dict = json.load(fin)
-
-        # Load the duckietown-lx repo branches into the form
-        schema["schema"]["branch"]["enum"] = get_branches("duckietown", "duckietown-lx")
-
-        values: Optional[dict] = open_form(
+        form_values: Optional[dict] = open_form_from_schema(
             shell,
-            schema=schema,
+            "lx-publish",
+            "v3",
             title="Publish your Learning Experience",
-            subtitle="Populate the fields below to publish your Learning Experience to the Duckietown LX repository",
-            icon_fpath=os.path.join(assets_dir, "icon.png"),
+            subtitle="Populate the fields below to publish your Learning Experience",
+            completion_message="Uploading your LX ...\n You can now close this page and return to the terminal."
         )
 
         # TODO: Pushing an ind. dir to the repo is non-trivial with the api. Do we need this?
---
+++ lx/publish/constants.py
@@ -0,0 +1,5 @@
+REQUIRED_DIR_TYPES = {
+    "meat": "template-exercise",
+    "recipe": "template-exercise-recipe",
+    "solution": "template-exercise"
+}
