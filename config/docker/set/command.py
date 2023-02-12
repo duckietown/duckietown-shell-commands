f7bb23ab854e48ccd1d4878aa0595eef66e37d09
Jason Hu <jasonhu515@gmail.com>
2023-02-12 18:13:34 +0100

    remove unused, adjust options and naming

--- config/docker/set/command.py
+++ config/docker/set/command.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 from typing import List
 
 from dt_shell import DTCommandAbs, dtslogger, DTShell
@@ -11,29 +10,32 @@ class DTCommand(DTCommandAbs):
     usage = f"""
 Usage:
 
-    dts config docker --docker-username <your-docker-username> --docker-password <your-docker-access-token> [--docker-registry <optional-specific-registry>]
+    dts config docker --username <your-docker-username> --password <your-docker-access-token> [optional-docker-server]
 """
 
     @staticmethod
     def command(shell, args, **kwargs):
-        parser = argparse.ArgumentParser(prog="dts config docker")
-        parser.add_argument(
-            "--docker-registry",
-            "--docker-server",
-            dest="server",
-            help="Docker server",
-            default="docker.io",
+        parser = argparse.ArgumentParser(
+            prog="dts config docker",
+            description=(
+                "Save Docker logins locally for Duckietown use. "
+                "The credentials are stored in plain text in ~/.dt-shell/config.yaml."
+            ),
         )
         parser.add_argument(
-            "--docker-username",
-            dest="username",
+            "server",
+            help="Docker server (default: docker.io)",
+            default="docker.io",
+            nargs="?",
+        )
+        parser.add_argument(
+            "--username",
             help="Docker username",
             required=True,
         )
         parser.add_argument(
-            "--docker-password",
-            dest="password",
-            help="Docker password or Docker token",
+            "--password",
+            help="Docker access token (preferred) or Docker password",
             required=True,
         )
         parsed = parser.parse_args(args)
@@ -47,14 +49,12 @@ Usage:
             shell.shell_config.docker_credentials[server] = {}
         
         if username is None and password is None:
-            print(DTCommand.usage)
+            dtslogger.warning(DTCommand.usage)
             exit(4)
 
         if username is not None:
-            shell.shell_config.docker_username = username
             shell.shell_config.docker_credentials[server]["username"] = username
         if password is not None:
-            shell.shell_config.docker_password = password
             shell.shell_config.docker_credentials[server]["secret"] = password
 
         shell.save_config()
