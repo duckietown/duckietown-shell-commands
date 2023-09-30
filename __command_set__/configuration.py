a8ebda1ac9a4d739baef43c01637f2c5f63f3b02
Andrea F. Daniele <afdaniele@duckietown.com>
2023-09-30 01:26:34 -0400

    added __command_set__ dir

---
+++ __command_set__/configuration.py
@@ -0,0 +1,15 @@
+from typing import Optional, List
+
+from dt_shell.commands import DTCommandSetConfigurationAbs
+from dt_shell.environments import Python3Environment, ShellCommandEnvironmentAbs
+
+
+class DTCommandSetConfiguration(DTCommandSetConfigurationAbs):
+
+    @classmethod
+    def default_environment(cls, **kwargs) -> Optional[ShellCommandEnvironmentAbs]:
+        """
+        The environment in which the commands in this set will run.
+        """
+        # return DockerContainerEnvironment("duckietown/dt-shell-commands-environment:ente")
+        return Python3Environment()
