2bc4ecbf1dd30d989d456029e6f74c469d75e95b
Andrea F. Daniele <afdaniele@duckietown.com>
2023-09-30 01:22:59 -0400

    removed core commands

--- version/command.py
+++
@@ -1,9 +0,0 @@
-from dt_shell import __version__, DTCommandAbs, DTShell
-
-
-class DTCommand(DTCommandAbs):
-    help = "Prints out the version of the shell and returns."
-
-    @staticmethod
-    def command(shell: DTShell, args):
-        print("dts version %s" % __version__)
