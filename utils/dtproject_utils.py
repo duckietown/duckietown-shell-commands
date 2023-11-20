650629ca4f13fdf57be40cba6810caeb9875fd5d
tuxliri <dvde.iafrate98@gmail.com>
2023-11-20 20:02:16 +0100

    Fixed handling of failed commands

--- utils/dtproject_utils.py
+++ utils/dtproject_utils.py
@@ -5,14 +5,19 @@ from cli.command import _run_cmd
 def _run_hooks(hook_name : str, project : DTProject):
     # Execute post-build hook
     if project.format.version >= 4:
-        for hook in project.hooks.hooks[hook_name]:
+        for hook in project.hooks[hook_name]:
             dtslogger.debug(f"Executing {hook_name} hook: {hook.command}")
-            success = _run_cmd(hook.command, return_exitcode=True)
 
-            if not success and hook.required:
-                dtslogger.error(f"{hook_name} hook failed! Aborting...")
+            try:
+                failed = _run_cmd(hook.command.split(), return_exitcode=True)
+            except FileNotFoundError:
+                dtslogger.error(f"Hook command '{hook.command}' not found! Aborting...")
+                failed = True
+
+            if failed and hook.required:
+                dtslogger.error(f"Required {hook_name} hook failed! Aborting...")
                 exit(1)
-            elif not success:
+            elif failed:
                 dtslogger.warning(f"{hook_name} hook failed! Continuing...")
     else:
         dtslogger.warning(f"Project format version {project.format.version} does not support hooks. Skipping...")
