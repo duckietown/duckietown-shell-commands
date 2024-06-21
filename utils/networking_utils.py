1b5f5af33017abeb923fa184307d0649531b3d2d
Andrea F. Daniele <afdaniele@duckietown.com>
2024-06-21 18:26:35 -0400

    minor

--- utils/networking_utils.py
+++ utils/networking_utils.py
@@ -1,7 +1,11 @@
 import os
+import platform
 import re
 import socket
+import subprocess
+from functools import lru_cache
 
+from dt_shell import dtslogger
 from utils.exceptions import NetworkingError
 
 
@@ -42,3 +46,45 @@ def resolve_hostname(hostname: str) -> str:
         msg = f"Failed to resolve host using name '{hostname}'.\n\tException(socket.gaierror): {e}"
         raise NetworkingError(msg)
     return protocol + ip + port
+
+
+def get_default_gateway_and_interface():
+    if platform.system() == "Darwin":
+        route_default_result = subprocess.check_output(["route", "get", "default"])
+        route_default_result = route_default_result.decode("utf-8")
+        gateway = re.search(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", route_default_result).group(0)
+        default_interface = re.search(r"(?:interface:.)(.*)", route_default_result).group(1)
+    elif platform.system() == "Linux":
+        route_default_result = subprocess.check_output(["ip", "route"])
+        route_default_result = route_default_result.decode("utf-8")
+        route_default_result = re.findall(r"([\w.][\w.]*'?\w?)", route_default_result)
+        gateway = route_default_result[2]
+        default_interface = route_default_result[4]
+    else:
+        print("(x) Could not read default routes.")
+        return None, None
+    # ---
+    if route_default_result:
+        return gateway, default_interface
+
+
+def get_interface_ip_address(ifname):
+    return os.popen(f'ip addr show {ifname}' + ' | grep "\<inet\>" | awk \'{ print $2 }\' '
+                                               '| awk -F "/" \'{ print $1 }\'').read().strip()
+
+
+@lru_cache
+def best_host_for_robot(robot: str, allow_static: bool = True) -> str:
+    mdns: str = f"{robot}.local" if not robot.endswith(".local") else robot
+    # try to get the IP address first (this is a static option)
+    if allow_static:
+        try:
+            ip = socket.gethostbyname(mdns)
+            # ---
+            dtslogger.debug(f"Best host for robot '{robot}' is its IP address '{ip}' (static)")
+            return ip
+        except socket.gaierror:
+            dtslogger.debug(f"Failed to resolve IP address from mDNS name '{mdns}'.")
+    # ---
+    dtslogger.debug(f"Best host for robot '{robot}' is its local mDNS name '{mdns}'")
+    return mdns
