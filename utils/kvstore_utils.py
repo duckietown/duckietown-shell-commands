82e9a1db206f17b0daec1244e60865d03eb38c7b
Andrea F. Daniele <afdaniele@duckietown.com>
2024-06-21 18:26:42 -0400

    updated kvstore_utils

--- utils/kvstore_utils.py
+++ utils/kvstore_utils.py
@@ -5,6 +5,8 @@ import cbor2
 import requests
 
 from dt_shell import dtslogger
+from duckietown_messages.base import BaseMessage
+from utils.networking_utils import best_host_for_robot
 
 KVSTORE_URL = "http://{host}:{port}/{path}/"
 KVSTORE_DEFAULT_PORT = 11411
@@ -17,7 +19,8 @@ class KVStore:
 
     def __init__(self, robot: str, port: int = KVSTORE_DEFAULT_PORT):
         self._robot: str = robot
-        self.url = KVSTORE_URL.format(host=f"{robot}.local", port=port, path="{path}")
+        host: str = best_host_for_robot(robot)
+        self.url = KVSTORE_URL.format(host=host, port=port, path="{path}")
 
     def _url(self, path: str):
         return self.url.format(path=path.strip("/"))
@@ -55,15 +58,25 @@ class KVStore:
             raise ValueError(msg)
         return native
 
-    def set(self, key: str, value: T, persist: bool = False, fail_quietly: bool = False):
+    def set(self, key: str, value: Union[BaseMessage, dict, list, str, int, float, bool, bytes], persist: bool = False,
+            fail_quietly: bool = False):
+        # base messages can be turned into dicts
+        if isinstance(value, BaseMessage):
+            value = value.dict()
+        # ---
         if self.has(key):
             # update existing field
             url = self._url(f"data/{key}")
-            data = cbor2.dumps(value)
             dtslogger.debug(f"KVStore[{self._robot}]: Updating key '{key}' to value '{value}'")
-            dtslogger.debug(f"$ POST {url}\n\t{data}")
-            response = requests.post(url, data=data)
-            dtslogger.debug(f" << {response.status_code}: {response.text}")
+            dtslogger.debug(f"$ POST {url}\n"
+                            f"--- request body ---\n"
+                            f"{value}\n"
+                            f"--------------------")
+            response = requests.post(url, json=value)
+            dtslogger.debug(f"$ POST {url}\n"
+                            f"--- response [{response.status_code}] ---  (url: {url})\n"
+                            f"{response.text}\n"
+                            f"----------------------")
             if not fail_quietly:
                 response.raise_for_status()
         else:
