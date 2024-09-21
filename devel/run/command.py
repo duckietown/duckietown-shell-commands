f2b0cb70ac28ef9411c8d68b972ef4a88bfda3de
Andrea F. Daniele <afdaniele@duckietown.com>
2024-09-21 16:50:07 -0400

    devel/run: improved with option for loading container configurations from the containers.yaml layer

--- devel/run/command.py
+++ devel/run/command.py
@@ -5,7 +5,10 @@ import json
 import os
 import shutil
 import subprocess
-from typing import List, Optional
+import tempfile
+from typing import List, Optional, Dict, Tuple
+
+import yaml
 
 from dt_shell import DTCommandAbs, dtslogger
 
@@ -14,20 +17,21 @@ from dtproject.constants import (
     BUILD_COMPATIBILITY_MAP,
     CANONICAL_ARCH
 )
+from dtproject.types import LayerContainers, ContainerConfiguration
 from utils.cli_utils import ensure_command_is_installed
 from utils.docker_utils import (
     DEFAULT_MACHINE,
     DOCKER_INFO,
     get_endpoint_architecture,
-    get_registry_to_use, CLOUD_BUILDERS, get_cloud_builder,
+    get_registry_to_use, CLOUD_BUILDERS, get_cloud_builder, merge_docker_compose_services,
 )
-from utils.misc_utils import human_size, sanitize_hostname
+from utils.misc_utils import human_size, sanitize_hostname, pretty_exc, pretty_yaml, random_string
 from utils.multi_command_utils import MultiCommand
 
 from .configuration import DEFAULT_TRUE
 
 LAUNCHER_FMT = "dt-launcher-%s"
-DEFAULT_MOUNTS = ["/var/run/avahi-daemon/socket", "/data"]
+DEFAULT_MOUNTS = ["/var/run/avahi-daemon/socket", "/data/"]
 REMOTE_USER = "duckie"
 REMOTE_GROUP = "duckie"
 
@@ -36,33 +40,48 @@ class DTCommand(DTCommandAbs):
     help = "Runs the current project"
 
     @staticmethod
-    def command(shell, args: list):
-        parser: argparse.ArgumentParser = DTCommand.parser
-        # try to interpret it as a multi-command
-        multi = MultiCommand(DTCommand, shell, [("-H", "--machine")], args)
-        if multi.is_multicommand:
-            multi.execute()
-            return
-        # everything after "++" is a passthrough for the container's command
+    def command(shell, args: list, **kwargs):
+        # get pre-parsed or parse arguments
+        parsed = kwargs.get("parsed", None)
         container_cmd_arguments: Optional[List[str]] = None
-        if "++" in args:
-            idx: int = args.index("++")
-            container_cmd_arguments = args[idx+1:]
-            args = args[:idx]
-        # add a fake positional argument to avoid missing the first argument starting with `-`
-        try:
-            idx = args.index("--")
-            args = args[:idx] + ["--", "--fake"] + args[idx + 1:]
-        except ValueError:
-            pass
-        # parse arguments
-        parsed, _ = parser.parse_known_args(args=args)
+        if parsed is None:
+            # parse arguments
+            parsed, _ = DTCommand.parser.parse_known_args(args=args)
+            # try to interpret it as a multi-command
+            multi = MultiCommand(DTCommand, shell, [("-H", "--machine")], args)
+            if multi.is_multicommand:
+                multi.execute()
+                return
+            # everything after "++" is a passthrough for the container's command
+            if "++" in args:
+                idx: int = args.index("++")
+                container_cmd_arguments = args[idx+1:]
+                args = args[:idx]
+            # add a fake positional argument to avoid missing the first argument starting with `-`
+            try:
+                idx = args.index("--")
+                args = args[:idx] + ["--", "--fake"] + args[idx + 1:]
+            except ValueError:
+                pass
+        else:
+            # combine given args with default values
+            default_parsed = DTCommand.parser.parse_args(args=[])
+            for k, v in parsed.__dict__.items():
+                setattr(default_parsed, k, v)
+            parsed = default_parsed
+
         # ---
         parsed.workdir = os.path.abspath(parsed.workdir)
 
         # incompatible args
+        # - mount and no-mount
         if isinstance(parsed.mount, (bool, str)) and parsed.mount and parsed.no_mount:
             dtslogger.error("You cannot both -m/--mount and --no-mount at the same time.")
+            exit(1)
+
+        # - docker args and configuration
+        if parsed.configuration is not None and parsed.docker_args:
+            dtslogger.error("You cannot use positional arguments together with -g/--configuration.")
             exit(1)
 
         # we mount the code inside the container by default unless we are using --cloud
@@ -159,17 +178,19 @@ class DTCommand(DTCommandAbs):
             dtslogger.info(f"Overriding version {version!r} with {parsed.tag!r}")
             version = parsed.tag
 
-        # get the module configuration
-        # noinspection PyListCreation
-        module_configuration_args = []
-        # apply default module configuration
-        module_configuration_args.append(f"--net={parsed.network_mode}")
+        # container config - network mode
+        cc_network_mode: str = parsed.network_mode
+        # container config - environment
+        cc_environment: Dict[str, str] = {}
+
         # environment
         if parsed.robot is not None:
-            module_configuration_args.append(f"-e=VEHICLE_NAME={parsed.robot}")
+            cc_environment["VEHICLE_NAME"] = parsed.robot
+
         # parse arguments
         mount_code = parsed.mount is True or isinstance(parsed.mount, str)
-        mount_option = []
+
+        cc_mountpoints: List[Tuple[str, str, str]] = []
 
         # add default mount points
         for mountpoint in DEFAULT_MOUNTS:
@@ -181,7 +202,7 @@ class DTCommand(DTCommandAbs):
                         f"This can create issues inside the container."
                     )
                     continue
-            mount_option += ["-v", "{:s}:{:s}".format(mountpoint, mountpoint)]
+            cc_mountpoints.append((mountpoint, mountpoint, "rw"))
 
         # mount source code (if requested)
         if mount_code:
@@ -211,9 +232,9 @@ class DTCommand(DTCommandAbs):
                     # compile mountpoints
                     for local_src, destination_src in zip(local_srcs, destination_srcs):
                         if parsed.read_write:
-                            mount_option += ["-v", "{:s}:{:s}:rw".format(local_src, destination_src)]
+                            cc_mountpoints.append((local_src, destination_src, "rw"))
                         else:
-                            mount_option += ["-v", "{:s}:{:s}:ro".format(local_src, destination_src)]
+                            cc_mountpoints.append((local_src, destination_src, "ro"))
 
                 # mount launchers
                 if not parsed.no_mount_launchers:
@@ -224,9 +245,14 @@ class DTCommand(DTCommandAbs):
                         destination_launchs = [destination_launchs]
                     # compile mountpoints
                     for local_launch, destination_launch in zip(local_launchs, destination_launchs):
-                        mount_option += ["-v", "{:s}:{:s}".format(local_launch, destination_launch)]
+                        # mount only if the launchers dir exists
+                        if not os.path.isdir(local_launch):
+                            continue
+
+                        cc_mountpoints.append((local_launch, destination_launch, "rw"))
                         # make sure the launchers are executable (local only)
                         if local:
+                            # noinspection PyBroadException
                             try:
                                 _run_cmd(["chmod", "a+x", os.path.join(local_launch, "*")], shell=True)
                             except Exception:
@@ -247,10 +273,10 @@ class DTCommand(DTCommandAbs):
                                 os.makedirs(local_mountpoint, exist_ok=True)
                                 real_local_lib = os.path.realpath(local_lib)
                                 destination_lib: str = os.path.join(destination_src, "libraries", f"__{lib_name}")
-                                mount_option += ["-v", "{:s}:{:s}".format(real_local_lib, destination_lib)]
+                                cc_mountpoints.append((real_local_lib, destination_lib, "rw"))
 
         # create image name
-        image = project.image(
+        cc_image = project.image(
             arch=parsed.arch,
             loop=parsed.loop,
             registry=registry_to_use,
@@ -282,6 +308,7 @@ class DTCommand(DTCommandAbs):
             compatible_archs = BUILD_COMPATIBILITY_MAP[CANONICAL_ARCH[epoint["Architecture"]]]
             if parsed.arch not in compatible_archs:
                 dtslogger.info("Configuring machine for multiarch...")
+                # noinspection PyBroadException
                 try:
                     _run_cmd(
                         [
@@ -296,9 +323,10 @@ class DTCommand(DTCommandAbs):
                         True,
                     )
                     dtslogger.info("Multiarch Enabled!")
-                except:
+                except Exception as e:
                     msg = "Multiarch cannot be enabled on the target machine. This might create issues."
                     dtslogger.warning(msg)
+                    dtslogger.debug("An exception occurred:\n" + pretty_exc(e, 4))
             else:
                 msg = "Running an image for {} on {}. Multiarch not needed!".format(
                     parsed.arch, epoint["Architecture"]
@@ -309,6 +337,7 @@ class DTCommand(DTCommandAbs):
         if parsed.pull or parsed.force_pull:
             # check if the endpoint contains an image with the same name
             is_present = False
+            # noinspection PyBroadException
             try:
                 out = _run_cmd(
                     ["docker", "-H=%s" % parsed.machine, "images", "--format", "{{.Repository}}:{{.Tag}}"],
@@ -316,24 +345,25 @@ class DTCommand(DTCommandAbs):
                     print_output=False,
                     suppress_errors=True,
                 )
-                is_present = image in out
-            except:
-                pass
+                is_present = cc_image in out
+            except Exception as e:
+                dtslogger.debug("An exception occurred:\n" + pretty_exc(e, 4))
             if not is_present or parsed.force_pull:
                 # try to pull the image
-                dtslogger.info('Pulling image "%s"...' % image)
+                dtslogger.info(f'Pulling image "{cc_image}"...')
+                # noinspection PyBroadException
                 try:
                     _run_cmd(
-                        ["docker", "-H=%s" % parsed.machine, "pull", image],
+                        ["docker", "-H=%s" % parsed.machine, "pull", cc_image],
                         get_output=True,
                         print_output=True,
                         suppress_errors=True,
                     )
-                except:
+                except Exception as e:
                     dtslogger.warning(
-                        'An error occurred while pulling the image "%s", maybe the image does not exist'
-                        % image
+                        f'An error occurred while pulling the image "{cc_image}", maybe the image does not exist'
                     )
+                    dtslogger.debug("An exception occurred:\n" + pretty_exc(e, 4))
             else:
                 dtslogger.info(
                     "Found an image with the same name. Using it. User --force-pull to force a new pull."
@@ -344,34 +374,35 @@ class DTCommand(DTCommandAbs):
             raise ValueError("You cannot use the option --launcher together with --cmd.")
         if parsed.launcher:
             parsed.cmd = LAUNCHER_FMT % parsed.launcher
-        cmd_option = [] if not parsed.cmd else [parsed.cmd]
-        cmd_arguments = (
+
+        cc_command: List[str] = [] if not parsed.cmd else [parsed.cmd]
+        cc_command_arguments = (
             [] if not container_cmd_arguments else
-            (["--"] if not cmd_option else []) + container_cmd_arguments
+            (["--"] if not cc_command else []) + container_cmd_arguments
         )
 
         # environment
         if parsed.machine == DEFAULT_MACHINE and not parsed.no_impersonate:
             host_uid: int = os.getuid()
             dtslogger.info(f"Impersonating host user with UID {host_uid}")
-            module_configuration_args += [
-                f"-e=IMPERSONATE_UID={host_uid}",
-                # NOTE: it is important to leave the container user's GID so that he can access his old files
-            ]
+            # NOTE: it is important to leave the container user's GID so that he can access his old files
+            cc_environment["IMPERSONATE_UID"] = str(host_uid)
 
         # docker arguments
-        if not parsed.docker_args:
-            parsed.docker_args = []
+        cc_remove: bool = False
+        cc_detach: bool = False
+        cc_docker_args: List[str] = parsed.docker_args or []
+
         if (not parsed.no_rm) or (not parsed.detach):
-            parsed.docker_args += ["--rm"]
+            cc_remove = True
         if parsed.detach:
-            parsed.docker_args += ["-d"]
+            cc_detach = True
 
         # add container name to docker args
-        parsed.docker_args += ["--name", parsed.name]
+        cc_name: str = parsed.name
 
         # escape spaces in arguments
-        parsed.docker_args = [a.replace(" ", "\\ ") for a in parsed.docker_args]
+        cc_docker_args = [a.replace(" ", "\\ ") for a in cc_docker_args]
 
         # sync
         if parsed.sync:
@@ -393,29 +424,111 @@ class DTCommand(DTCommandAbs):
                 )
             # run rsync
             for project_path in projects_to_sync:
-                cmd = f"rsync --archive --delete --copy-links --chown={REMOTE_USER}:{REMOTE_GROUP} {project_path} {remote_path}"
+                cmd = (f"rsync --archive --delete --copy-links --chown={REMOTE_USER}:{REMOTE_GROUP} "
+                       f"\"{project_path}\" \"{remote_path}\"")
                 _run_cmd(cmd, shell=True)
             dtslogger.info(f"Code synced!")
 
         # run
-        exitcode = _run_cmd(
-            [parsed.runtime, "-H=%s" % parsed.machine, "run", "-it"]
-            + module_configuration_args
-            + parsed.docker_args
-            + mount_option
-            + [image]
-            + cmd_option
-            + cmd_arguments,
-            suppress_errors=True,
-            return_exitcode=True,
-        )
-        dtslogger.debug(f"Command exited with exit code [{exitcode}].")
-        if parsed.detach:
-            dtslogger.info("Your container is running in detached mode!")
+        if parsed.configuration is None:
+            # use docker CLI directly
+            exitcode = _run_cmd(
+                [parsed.runtime, "-H=%s" % parsed.machine, "run", "-it"]
+                + [f"--net={cc_network_mode}"]
+                + [f"-e={k}={v}" for k, v in cc_environment.items()]
+                + [f"-v={src}:{dst}:{mode}" for src, dst, mode in cc_mountpoints]
+                + (["--rm"] if cc_remove else [])
+                + (["-d"] if cc_detach else [])
+                + [f"--name={cc_name}"]
+                + cc_docker_args
+                + [cc_image]
+                + cc_command
+                + cc_command_arguments,
+                suppress_errors=True,
+                return_exitcode=True,
+            )
+            dtslogger.debug(f"Command exited with exit code [{exitcode}].")
+            if parsed.detach:
+                dtslogger.info("Your container is running in detached mode!")
+
+        else:
+            # use a temporary docker-compose file instead
+            assert len(cc_docker_args) == 0
+
+            # load configuration
+            container_configurations: LayerContainers = project.layers.containers
+            try:
+                base_cc: ContainerConfiguration = container_configurations[parsed.configuration]
+            except KeyError:
+                dtslogger.error(f"Container configuration '{parsed.configuration}' not found in project. "
+                                f"Valid container configurations are: {list(container_configurations.keys())}")
+                exit(1)
+            # create docker-compose configuration
+            docker_compose = args_to_docker_compose(
+                cc_image,
+                cc_name,
+                cc_network_mode,
+                cc_environment,
+                cc_mountpoints,
+                cc_command,
+                cc_command_arguments,
+                base_cc
+            )
+            # write temporary docker-compose.yaml file and run it
+            with tempfile.NamedTemporaryFile(mode="w", delete=True) as f:
+                # write docker-compose file
+                yaml.dump(docker_compose, f)
+                f.flush()
+                # run docker-compose
+                dtslogger.info("Running container using docker-compose...")
+                dtslogger.debug(f"Using docker-compose file [{f.name}] with configuration:\n"
+                                f"{pretty_yaml(docker_compose, indent=4)}")
+                # run docker-compose
+                exitcode = _run_cmd(
+                    [
+                        "docker", f"-H={parsed.machine}", "compose",
+                        "-f", f.name,
+                        "-p", f"dts-devel-run-{random_string(4)}",
+                        "up",
+                        "--exit-code-from", cc_name,
+                        "--abort-on-container-exit",
+                    ]
+                    + (["-d"] if cc_detach else []),
+                    suppress_errors=True,
+                    return_exitcode=True,
+                )
+                dtslogger.debug(f"Docker-compose exited with exit code [{exitcode}].")
 
     @staticmethod
     def complete(shell, word, line):
         return []
+
+
+def args_to_docker_compose(
+        image: str,
+        cc_name: str,
+        cc_network_mode: str,
+        cc_environment: Dict[str, str],
+        cc_mountpoints: List[Tuple[str, str, str]],
+        cc_command: List[str],
+        cc_command_arguments: List[str],
+        base_cc: ContainerConfiguration
+) -> dict:
+    runtime_cc = {
+        "image": image,
+        "network_mode": cc_network_mode,
+        "environment": cc_environment,
+        "volumes": [f"{src}:{dst}:{mode}" for src, dst, mode in cc_mountpoints],
+        "command": cc_command + cc_command_arguments,
+        "stdin_open": True,
+        "tty": True,
+    }
+    cfg = {
+        "services": {
+            cc_name: merge_docker_compose_services(base_cc.service, runtime_cc)
+        },
+    }
+    return cfg
 
 
 def _run_cmd(
