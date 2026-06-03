f40bc153f3517c9cc08f6d9c27ea1e4cbfc2520a
Liam McAlpine <liam.mcalpine@duckietown.com>
2026-06-03 07:36:26 +0000

    feat: add startup log streaming functionality for virtual robots

--- duckiebot/virtual/restart/command.py
+++ duckiebot/virtual/restart/command.py
@@ -12,6 +12,12 @@ class DTCommand(DTCommandAbs):
         prog = "dts duckiebot virtual restart"
         parser = argparse.ArgumentParser(prog=prog)
         # define arguments
+        parser.add_argument(
+            "--no-logs",
+            action="store_true",
+            default=False,
+            help="Do not stream startup logs",
+        )
         parser.add_argument("robot", nargs=1, help="Name of the Robot to restart")
         # parse arguments
         parsed = parser.parse_args(args)
@@ -24,5 +30,5 @@ class DTCommand(DTCommandAbs):
         time.sleep(1)
         # start
         dtslogger.info(f"Starting up virtual robot '{parsed.robot}'...")
-        shell.include.duckiebot.virtual.start.command(shell, [parsed.robot])
+        shell.include.duckiebot.virtual.start.command(shell, [parsed.robot, "--no-logs"] if parsed.no_logs else [parsed.robot])
         return True
