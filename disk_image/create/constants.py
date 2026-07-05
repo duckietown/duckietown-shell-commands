a0d5f9aaee454d23614c64c4e87bac7b303c1943
Liam McAlpine <liam.mcalpine@duckietown.com>
2026-07-05 08:06:02 +0000

    feat: enhance temporary working directory handling and add utility functions

--- disk_image/create/constants.py
+++ disk_image/create/constants.py
@@ -1,7 +1,39 @@
+import os
+
+
 PARTITION_MOUNTPOINT = lambda partition: f"/media/dts/{partition}"
 DISK_DEVICE = lambda device, partition_id: f"{device}p{partition_id}"
 FILE_PLACEHOLDER_SIGNATURE = "DT_DUCKIETOWN_PLACEHOLDER_"
-TMP_WORKDIR = "/tmp/duckietown/dts/disk_image"
+LEGACY_TMP_WORKDIR = "/tmp/duckietown/dts/disk_image"
+HOME_DIR = os.path.expanduser("~")
+FALLBACK_TMP_WORKDIR = os.path.join(HOME_DIR, ".cache", "duckietown", "dts", "disk_image")
+
+
+def _get_existing_parent_dir(path: str) -> str:
+    parent_dir = path
+    while not os.path.isdir(parent_dir):
+        next_parent_dir = os.path.dirname(parent_dir)
+        if next_parent_dir == parent_dir:
+            break
+        parent_dir = next_parent_dir
+    return parent_dir
+
+
+def _can_create_workdir(path: str) -> bool:
+    existing_parent_dir = _get_existing_parent_dir(path)
+    required_mode = os.W_OK | os.X_OK
+    return os.access(existing_parent_dir, required_mode)
+
+
+def _get_tmp_workdir() -> str:
+    candidate_dirs = (LEGACY_TMP_WORKDIR, FALLBACK_TMP_WORKDIR)
+    for workdir in candidate_dirs:
+        if _can_create_workdir(workdir):
+            return workdir
+    raise OSError("Could not determine a writable disk image working directory.")
+
+
+TMP_WORKDIR = _get_tmp_workdir()
 DISK_IMAGE_STATS_LOCATION = "data/stats/disk_image/build.json"
 DATA_STORAGE_DISK_IMAGE_DIR = "disk_image"
 DEFAULT_STACK = "duckietown"
